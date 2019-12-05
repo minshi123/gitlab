10d4298ef83e3df5bcfacdf0127e220462dae883
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-12-05 20:07:36 +0000

    Connect designs-at-version to versions

--- ee/app/graphql/types/design_management/version_type.rb
+++ ee/app/graphql/types/design_management/version_type.rb
@@ -2,7 +2,7 @@
 
 module Types
   module DesignManagement
-    class VersionType < BaseObject
+    class VersionType < ::Types::BaseObject
       # Just `Version` might be a bit to general to expose globally so adding
       # a `Design` prefix to specify the class exposed in GraphQL
       graphql_name 'DesignVersion'
@@ -13,8 +13,23 @@ module Types
             description: 'ID of the design version'
       field :sha, GraphQL::ID_TYPE, null: false,
             description: 'SHA of the design version'
-      field :designs, Types::DesignManagement::DesignType.connection_type, null: false,
+
+      field :designs,
+            ::Types::DesignManagement::DesignType.connection_type,
+            null: false,
             description: 'All designs that were changed in the version'
+
+      field :designs_at_version,
+            ::Types::DesignManagement::DesignAtVersionType.connection_type,
+            null: false,
+            description: 'All designs visible at this version as-of this version',
+            resolver: ::Resolvers::DesignManagement::Version::DesignsAtVersionResolver
+
+      field :design_at_version,
+            ::Types::DesignManagement::DesignAtVersionType,
+            null: false,
+            description: 'A particular design visible at this version as-of this version',
+            resolver: ::Resolvers::DesignManagement::Version::DesignsAtVersionResolver.single
     end
   end
 end
