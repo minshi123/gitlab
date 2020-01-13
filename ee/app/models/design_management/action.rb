c412c5fa7483378369ecaa3cf1b527f01d1c1f69
Luke Duncalfe <lduncalfe@eml.cc>
2020-01-13 17:12:08 +1300

    Generate smaller image sizes for Designs
    Adds a new DesignUploader to generate and serve smaller versions of
    design files.
    
    The original (full-sized) design files are stored in Git LFS, and so
    have a different uploader, `LfsObjectUploader`.
    
    The decision to make a separate uploader to handle just the smaller
    image versions came out of a discussion in
    https://gitlab.com/gitlab-org/gitlab/merge_requests/20511. Initially,
    the direction taken was to make LfsObjectUploader conditionally generate
    smaller images for LfsObject files. The feedback was that this was
    adding logic that was too specific to design management into an uploader
    that is very general, and that we should use a separate uploader to
    handle just the smaller image versions.
    
    https://gitlab.com/gitlab-org/gitlab/issues/12577
    https://gitlab.com/gitlab-org/gitlab/issues/13815

--- ee/app/models/design_management/action.rb
+++ ee/app/models/design_management/action.rb
@@ -4,6 +4,8 @@ module DesignManagement
   class Action < ApplicationRecord
     self.table_name = "#{DesignManagement.table_name_prefix}designs_versions"
 
+    mount_uploader :file, DesignManagement::DesignUploader
+
     belongs_to :design, class_name: "DesignManagement::Design", inverse_of: :actions
     belongs_to :version, class_name: "DesignManagement::Version", inverse_of: :actions
 
