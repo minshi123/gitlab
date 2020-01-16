8beca57d78fe29ecaeb7e46573f3cac15189e878
David Fernandez <dfernandez@gitlab.com>
2020-01-16 11:52:36 +0100

    Add support to seed all package types

--- ee/db/fixtures/development/26_packages.rb
+++ ee/db/fixtures/development/26_packages.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 class Gitlab::Seeder::Packages
+  attr_reader :user, :project
+
   def initialize(user, project)
     @user = user
     @project = project
   end
 
-  def seed
+  def seed_npm_packages
     5.times do |i|
-      package_name = "@#{@project.full_path}"
+      package_name = "@#{@project.full_path}-npm"
       version = "1.12.#{i}"
+
+      fixture_json = File.read(Rails.root.join('ee', 'spec', 'fixtures', 'npm', 'payload.json'))
 
       params = JSON.parse(
         fixture_json
@@ -16,23 +20,60 @@ class Gitlab::Seeder::Packages
           .gsub('1.0.1', version))
         .with_indifferent_access
 
-      ::Packages::Npm::CreatePackageService.new(@project, @user, params).execute
+      ::Packages::Npm::CreatePackageService.new(project, user, params).execute
+      print '.'
     end
   end
 
-  private
+  def seed_maven_packages
+    5.times do |i|
+      path = "my/company/app/maven-app"
+      version = "1.0.#{i}-SNAPSHOT"
 
-  def fixture_json
-    File.read(fixture_path)
+      params = {
+        path: "#{path}/#{version}",
+        name: path,
+        version: version
+      }
+
+      ::Packages::CreateMavenPackageService.new(project, user, params).execute
+      print '.'
+    end
   end
 
-  def fixture_path
-    Rails.root.join('ee', 'spec', 'fixtures', 'npm', 'payload.json')
+  def seed_conan_packages
+    5.times do |i|
+      params = {
+        package_name: 'my-conan-pkg',
+        package_version: "1.0.#{i}",
+        package_username: ::Packages::ConanMetadatum.package_username_from(full_path: project.full_path),
+        package_channel: 'stable'
+      }
+
+      ::Packages::Conan::CreatePackageService.new(project, user, params).execute
+      print '.'
+    end
+  end
+
+  def seed_nuget_packages
+    5.times do |i|
+      pkg = ::Packages::Nuget::CreatePackageService.new(project, user, {}).execute
+      pkg.update!(name: 'MyNugetApp.Package', version: "1.4.#{i}")
+      print '.'
+    end
   end
 end
 
 Gitlab::Seeder.quiet do
+  flag = 'SEED_ALL_PACKAGE_TYPES'
+
+  package_types = ENV[flag] ? %i[npm maven conan nuget] : [:npm]
+
   Project.not_mass_generated.sample(5).each do |project|
-    Gitlab::Seeder::Packages.new(project.owner, project).seed
+    puts "\nSeeding packages for the '#{project.full_path}' project"
+
+    package_types.each do |package_type|
+      Gitlab::Seeder::Packages.new(project.owner, project).send("seed_#{package_type}_packages")
+    end
   end
 end
