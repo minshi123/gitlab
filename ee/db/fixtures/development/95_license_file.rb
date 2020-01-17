0672bf0e638f769b3660ffdf52f434a32b06fbfa
David Fernandez <dfernandez@gitlab.com>
2020-01-17 16:35:14 +0100

    Add seed for license file

---
+++ ee/db/fixtures/development/95_license_file.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Gitlab::Seeder::LicenseFile
+  attr_reader :file_path
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def seed!
+    if File.file?(file_path)
+      file = File.open(file_path)
+
+      ::License.create!(data_file: file)
+    else
+      puts "Skipped. Given file path #{ENV[flag]} is not a file."
+    end
+  end
+end
+
+Gitlab::Seeder.quiet do
+  flag = 'SEED_LICENSE_FILE_PATH'
+
+  if ENV[flag]
+    seeder = Gitlab::Seeder::LicenseFile.new(ENV[flag])
+    seeder.seed!
+
+    puts "Seeded License file from #{ENV[flag]}."
+  else
+    puts "Skipped. Use the `#{flag}` environment variable to seed the license file of the given path."
+  end
+end
