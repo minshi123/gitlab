e57743dd39872ce593682a34afeb28a01c9787f1
Paul Slaughter <pslaughter@gitlab.com>
2020-01-27 15:16:35 -0600

    Refactored recursive part of file_row to file_tree
    **Notes:**
    - Also introduces IDE and Diff specific file_row
    
    **Next step:**
    - Get rid of `extraComponent` prop (replace with slot)
    - Get rid of confusing show/hide props
    - Update tests :)

---
+++ app/assets/javascripts/diffs/components/diff_file_row.vue
@@ -0,0 +1,13 @@
+<script>
+import FileRow from '~/vue_shared/components/file_row.vue';
+
+export default {
+  components: {
+    FileRow,
+  },
+};
+</script>
+
+<template>
+  <file-row v-bind="$attrs" v-on="$listeners" />
+</template>
