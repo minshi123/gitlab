9762667484fbbbc7a0da7c806d7bdd2ba404ffe4
Matthias Kaeppler <mkaeppler@gitlab.com>
2020-01-31 14:19:19 +0100

    BulkInsertable guards against bulk-unsafe calls
    This mixin overrides certain active record calls that modify the
    callback chain in ways incompatible with bulk inserts

---
+++ app/models/concerns/bulk_insertable.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module BulkInsertable
+  IncludeNotAllowedError = Class.new(StandardError)
+  MethodDefinitionNotAllowedError = Class.new(StandardError)
+
+  def self.included(mod)
+    raise IncludeNotAllowedError.new("You must `extend` the `#{self}` module")
+  end
+
+  BLACKLISTED_METHODS = [
+    :before_save, :after_save, :before_create, :after_create,
+    :before_commit, :after_commit, :around_save, :around_create,
+    :before_validation, :after_validation, :validate, :validates
+  ].freeze
+
+  BLACKLISTED_METHODS.each do |method|
+    define_method(method) do |*args|
+      return super(args) if _bi_allow_method?(method, args)
+
+      raise MethodDefinitionNotAllowedError.new(
+        "Not allowed to call `#{method}(args: #{args.inspect})` when model < `BulkInsertable`")
+    end
+  end
+
+  private
+
+  def _bi_allow_method?(method, args)
+    _bi_before_save_called_from_belongs_to?(method, args)
+  end
+
+  def _bi_before_save_called_from_belongs_to?(method, args)
+    method == :before_save && args&.first.to_s.start_with?('autosave_associated_records_for_')
+  end
+end
