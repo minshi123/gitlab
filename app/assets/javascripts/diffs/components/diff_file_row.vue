8d5bb30effef16de472b121a8a233326638e0f5a
Samantha Ming <sming@gitlab.com>
2020-02-04 19:55:31 -0800

    Re-add file row stats to tree list
    - display file row stats in diff file list
    - wrote spec

--- app/assets/javascripts/diffs/components/diff_file_row.vue
+++ app/assets/javascripts/diffs/components/diff_file_row.vue
@@ -1,13 +1,24 @@
 <script>
 import FileRow from '~/vue_shared/components/file_row.vue';
+import FileRowStats from './file_row_stats.vue';
 
 export default {
+  name: 'DiffFileRow',
   components: {
     FileRow,
+    FileRowStats,
+  },
+  props: {
+    file: {
+      type: Object,
+      required: true,
+    },
   },
 };
 </script>
 
 <template>
-  <file-row v-bind="$attrs" v-on="$listeners" />
+  <file-row :file="file" v-bind="$attrs" v-on="$listeners">
+    <file-row-stats v-if="file.type === 'blob'" :file="file" class="mr-1" />
+  </file-row>
 </template>
--- spec/frontend/diffs/components/diff_file_row_spec.js
+++ spec/frontend/diffs/components/diff_file_row_spec.js
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import DiffFileRow from '~/diffs/components/diff_file_row.vue';
 import FileRow from '~/vue_shared/components/file_row.vue';
+import FileRowStats from '~/diffs/components/file_row_stats.vue';
 
 describe('Diff File Row component', () => {
   let wrapper;
@@ -21,5 +22,21 @@ describe('Diff File Row component', () => {
       file: {},
     });
     expect(wrapper.find(FileRow).exists()).toEqual(true);
+  });
+
+  describe('FileRowStats components', () => {
+    it.each`
+      type       | value    | desc
+      ${'blob'}  | ${true}  | ${'is shown if file type is blob'}
+      ${'hello'} | ${false} | ${'is hidden if file is not blob'}
+    `('$desc', ({ type, value }) => {
+      createComponent({
+        level: 4,
+        file: {
+          type,
+        },
+      });
+      expect(wrapper.find(FileRowStats).exists()).toEqual(value);
+    });
   });
 });
