887b550ff32404572a12fecb972983548c804a2a
Daniel Tian <dtian@gitlab.com>
2020-02-04 18:58:59 -1000

    Add vulnerability management badge and fix status dropdown bug

--- ee/app/assets/javascripts/pages/projects/security/dashboard/show/index.js
+++ ee/app/assets/javascripts/pages/projects/security/dashboard/show/index.js
@@ -2,12 +2,20 @@ import App from 'ee/vulnerability_management/components/app.vue';
 import Vue from 'vue';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const el = document.getElementById('vulnerability-show-header');
-  const { state, id } = el.dataset;
+  const el = document.getElementById('vulnerability-management-app');
+  const { vulnerability, vulnerabilityUrl, pipeline, pipelineUrl } = el.dataset;
+
+  // Convert the vulnerability and pipeline JSON into objects and set their respective URLs.
+  const vulnerabilityObject = JSON.parse(vulnerability);
+  vulnerabilityObject.url = vulnerabilityUrl;
+  const pipelineObject = JSON.parse(pipeline);
+  pipelineObject.url = pipelineUrl;
 
   return new Vue({
     el,
-
-    render: h => h(App, { props: { state, id: Number(id) } }),
+    render: createElement =>
+      createElement(App, {
+        props: { vulnerability: vulnerabilityObject, pipeline: pipelineObject },
+      }),
   });
 });
