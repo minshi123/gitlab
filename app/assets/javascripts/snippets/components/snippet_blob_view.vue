10546bbb630dce98fdddd973a2aa465172953ce2
Denys Mishunov <dmishunov@gitlab.com>
2020-02-11 16:19:10 +0100

    Added Blob Content into Snippet Blob View
    Handle viewer switches
    
    If there's hash in the URL, switch to Simple view right away

--- app/assets/javascripts/snippets/components/snippet_blob_view.vue
+++ app/assets/javascripts/snippets/components/snippet_blob_view.vue
@@ -2,13 +2,19 @@
 import BlobEmbeddable from '~/blob/components/blob_embeddable.vue';
 import { SNIPPET_VISIBILITY_PUBLIC } from '../constants';
 import BlobHeader from '~/blob/components/blob_header.vue';
-import GetSnippetBlobQuery from '../queries/snippet.blob.query.graphql';
+import BlobContent from '~/blob/components/blob_content.vue';
 import { GlLoadingIcon } from '@gitlab/ui';
+
+import GetSnippetBlobQuery from '../queries/snippet.blob.query.graphql';
+import GetBlobContent from '../queries/snippet.blob.content.query.graphql';
+
+import { SIMPLE_BLOB_VIEWER, RICH_BLOB_VIEWER } from '~/blob/components/constants';
 
 export default {
   components: {
     BlobEmbeddable,
     BlobHeader,
+    BlobContent,
     GlLoadingIcon,
   },
   apollo: {
@@ -20,6 +26,26 @@ export default {
         };
       },
       update: data => data.snippets.edges[0].node.blob,
+      result(res) {
+        if (window.location.hash) {
+          this.activeViewerType = SIMPLE_BLOB_VIEWER;
+        } else {
+          this.activeViewerType = res.data.snippets.edges[0].node.blob.richViewer
+            ? RICH_BLOB_VIEWER
+            : SIMPLE_BLOB_VIEWER;
+        }
+      },
+    },
+    blobContent: {
+      query: GetBlobContent,
+      variables() {
+        return {
+          ids: this.snippet.id,
+          rich: this.activeViewerType === RICH_BLOB_VIEWER,
+        };
+      },
+      update: data =>
+        data.snippets.edges[0].node.blob.richData || data.snippets.edges[0].node.blob.plainData,
     },
   },
   props: {
@@ -31,6 +57,8 @@ export default {
   data() {
     return {
       blob: {},
+      blobContent: '',
+      activeViewerType: '',
     };
   },
   computed: {
@@ -39,6 +67,18 @@ export default {
     },
     isBlobLoading() {
       return this.$apollo.queries.blob.loading;
+    },
+    isContentLoading() {
+      return this.$apollo.queries.blobContent.loading;
+    },
+    viewer() {
+      const { richViewer, simpleViewer } = this.blob;
+      return this.activeViewerType === RICH_BLOB_VIEWER ? richViewer : simpleViewer;
+    },
+  },
+  methods: {
+    switchViewer(viewer) {
+      this.activeViewerType = viewer;
     },
   },
 };
@@ -53,7 +93,8 @@ export default {
       class="prepend-top-20 append-bottom-20"
     />
     <article v-else class="file-holder snippet-file-content">
-      <blob-header :blob="blob" />
+      <blob-header :blob="blob" :active-viewer-type="viewer.type" @viewer-changed="switchViewer" />
+      <blob-content :loading="isContentLoading" :content="blobContent" :active-viewer="viewer" />
     </article>
   </div>
 </template>
--- spec/frontend/snippets/components/snippet_blob_view_spec.js
+++ spec/frontend/snippets/components/snippet_blob_view_spec.js
@@ -3,11 +3,14 @@ import { GlLoadingIcon } from '@gitlab/ui';
 import SnippetBlobView from '~/snippets/components/snippet_blob_view.vue';
 import BlobHeader from '~/blob/components/blob_header.vue';
 import BlobEmbeddable from '~/blob/components/blob_embeddable.vue';
+import BlobContent from '~/blob/components/blob_content.vue';
 import {
   SNIPPET_VISIBILITY_PRIVATE,
   SNIPPET_VISIBILITY_INTERNAL,
   SNIPPET_VISIBILITY_PUBLIC,
 } from '~/snippets/constants';
+
+import { Blob as BlobMock, SimpleViewerMock, RichViewerMock } from 'jest/blob/components/mock_data';
 
 describe('Blob Embeddable', () => {
   let wrapper;
@@ -16,12 +19,24 @@ describe('Blob Embeddable', () => {
     webUrl: 'https://foo.bar',
     visibilityLevel: SNIPPET_VISIBILITY_PUBLIC,
   };
+  const dataMock = {
+    blob: BlobMock,
+    activeViewerType: SimpleViewerMock.type,
+  };
 
-  function createComponent(props = {}, loading = false) {
+  function createComponent(
+    props = {},
+    data = dataMock,
+    blobLoading = false,
+    contentLoading = false,
+  ) {
     const $apollo = {
       queries: {
         blob: {
-          loading,
+          loading: blobLoading,
+        },
+        blobContent: {
+          loading: contentLoading,
         },
       },
     };
@@ -32,6 +47,11 @@ describe('Blob Embeddable', () => {
           ...snippet,
           ...props,
         },
+      },
+      data() {
+        return {
+          ...data,
+        };
       },
       mocks: { $apollo },
     });
@@ -48,6 +68,7 @@ describe('Blob Embeddable', () => {
       createComponent();
       expect(wrapper.find(BlobEmbeddable).exists()).toBe(true);
       expect(wrapper.find(BlobHeader).exists()).toBe(true);
+      expect(wrapper.find(BlobContent).exists()).toBe(true);
     });
 
     it.each([SNIPPET_VISIBILITY_INTERNAL, SNIPPET_VISIBILITY_PRIVATE, 'foo'])(
@@ -68,9 +89,33 @@ describe('Blob Embeddable', () => {
     });
 
     it('shows loading icon while blob data is in flight', () => {
-      createComponent({}, true);
+      createComponent({}, dataMock, true);
       expect(wrapper.find(GlLoadingIcon).exists()).toBe(true);
       expect(wrapper.find('.snippet-file-content').exists()).toBe(false);
+    });
+
+    it('sets simple viewer correctly', () => {
+      createComponent();
+      expect(wrapper.vm.viewer).toEqual(SimpleViewerMock);
+    });
+
+    it('sets rich viewer correctly', () => {
+      const data = Object.assign({}, dataMock, {
+        activeViewerType: RichViewerMock.type,
+      });
+      createComponent({}, data);
+      expect(wrapper.vm.viewer).toEqual(RichViewerMock);
+    });
+
+    it('correctly switches viewer type', () => {
+      createComponent();
+      expect(wrapper.vm.viewer).toEqual(SimpleViewerMock);
+
+      wrapper.vm.switchViewer(RichViewerMock.type);
+      expect(wrapper.vm.viewer).toEqual(RichViewerMock);
+
+      wrapper.vm.switchViewer(SimpleViewerMock.type);
+      expect(wrapper.vm.viewer).toEqual(SimpleViewerMock);
     });
   });
 });
