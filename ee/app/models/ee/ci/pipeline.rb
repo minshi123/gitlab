15abe648d44f45f44a4869e13ea918bb40273eff
Tetiana Chupryna <tchupryna@gitlab.com>
2020-02-12 13:59:34 +0200

    Add license_scanning feature

--- ee/app/models/ee/ci/pipeline.rb
+++ ee/app/models/ee/ci/pipeline.rb
@@ -43,8 +43,8 @@ module EE
           container_scanning: %i[container_scanning sast_container],
           dast: %i[dast],
           performance: %i[merge_request_performance_metrics],
-          license_management: %i[license_management],
-          license_scanning: %i[license_management],
+          license_management: %i[license_management license_scanning],
+          license_scanning: %i[license_management license_scanning],
           metrics: %i[metrics_reports]
         }.freeze
 
