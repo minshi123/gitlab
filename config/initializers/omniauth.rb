44868ee9e8a8136e9db35557f94f6a1f827df074
Qingyu Zhao <qzhao@gitlab.com>
2020-02-17 10:50:28 +1100

    OmniAuth only log ERROR in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -19,3 +19,4 @@ end
 OmniAuth.config.logger.formatter = proc do |severity, datetime, progname, msg|
   { severity: severity, timestamp: datetime.utc.iso8601(3), pid: $$, progname: progname, message: msg }.to_json << "\n"
 end
+OmniAuth.config.logger.level = Logger::ERROR if Rails.env.production?
