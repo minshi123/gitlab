d0a666e04f33a8fa7495847b7bb77c3d9730456a
Miguel Rincon <mrincon@gitlab.com>
2020-02-19 21:00:08 +0800

    Convert dashobard keys to camel case
    When the action response is obtained, convert it before
    it arrives to the store.
    
    Some keys are ignore from the conversion to reduce scope of changes.
    
    Update mock_data to reduce ducplication.

--- spec/frontend/monitoring/store/utils_spec.js
+++ spec/frontend/monitoring/store/utils_spec.js
@@ -13,8 +13,10 @@ describe('normalizeMetric', () => {
     { args: [undefined], expected: 'undefined_undefined' },
     { args: [{ id: 'something' }], expected: 'undefined_something' },
     { args: [{ id: 45 }], expected: 'undefined_45' },
+    { args: [{ metricId: 5 }], expected: '5_undefined' },
     { args: [{ metric_id: 5 }], expected: '5_undefined' },
     { args: [{ metric_id: 'something' }], expected: 'something_undefined' },
+    { args: [{ metricId: 'something' }], expected: 'something_undefined' },
     {
       args: [{ metric_id: 5, id: 'system_metrics_kubernetes_container_memory_total' }],
       expected: '5_system_metrics_kubernetes_container_memory_total',
@@ -33,6 +35,11 @@ describe('uniqMetricsId', () => {
     { input: { metric_id: 2, id: 21 }, expected: '2_21' },
     { input: { metric_id: 22, id: 1 }, expected: '22_1' },
     { input: { metric_id: 'aaa', id: '_a' }, expected: 'aaa__a' },
+
+    { input: { metricId: 2 }, expected: '2_undefined' },
+    { input: { metricId: 2, id: 21 }, expected: '2_21' },
+    { input: { metricId: 22, id: 1 }, expected: '22_1' },
+    { input: { metricId: 'aaa', id: '_a' }, expected: 'aaa__a' },
   ].forEach(({ input, expected }) => {
     it(`creates unique metric ID with ${JSON.stringify(input)}`, () => {
       expect(uniqMetricsId(input)).toEqual(expected);
