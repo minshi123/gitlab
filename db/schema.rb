80b19b3dc157b5ee21534479c89556fc4f530f9c
Patrick Derichs <pderichs@gitlab.com>
2020-02-19 14:58:49 +0100

    Add limit_metric to lists table

--- db/schema.rb
+++ db/schema.rb
@@ -2384,10 +2384,11 @@ ActiveRecord::Schema.define(version: 2020_02_13_220211) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "milestone_id"
+    t.integer "user_id"
     t.integer "max_issue_count", default: 0, null: false
     t.integer "max_issue_weight", default: 0, null: false
+    t.integer "limit_metric"
     t.index ["board_id", "label_id"], name: "index_lists_on_board_id_and_label_id", unique: true
     t.index ["label_id"], name: "index_lists_on_label_id"
     t.index ["list_type"], name: "index_lists_on_list_type"
