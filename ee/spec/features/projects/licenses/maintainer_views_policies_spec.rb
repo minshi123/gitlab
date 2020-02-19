1678e17f95ae3a3fc68e2850bea58da2c3dcc2c6
mo khan <mo.khan@gmail.com>
2020-02-19 14:43:05 -0700

    Use selector to find a specific row

--- ee/spec/features/projects/licenses/maintainer_views_policies_spec.rb
+++ ee/spec/features/projects/licenses/maintainer_views_policies_spec.rb
@@ -14,11 +14,14 @@ describe 'EE > Projects > Licenses > Maintainer views policies' do
   before do
     sign_in(maintainer)
     stub_licensed_features(license_management: true)
-    visit path
   end
 
   context 'when policies are not configured' do
     let(:help_path) { help_page_path('user/application_security/license_compliance/index') }
+
+    before do
+      visit path
+    end
 
     it 'displays a link to the documentation to configure license compliance' do
       expect(page).to have_content('License Compliance')
@@ -30,17 +33,25 @@ describe 'EE > Projects > Licenses > Maintainer views policies' do
     let!(:mit) { create(:software_license, :mit) }
     let!(:mit_policy) { create(:software_license_policy, :denied, software_license: mit, project: project) }
     let!(:pipeline) { create(:ee_ci_pipeline, project: project, builds: [create(:ee_ci_build, :license_scan_v2, :success)]) }
+    let(:report) { JSON.parse(fixture_file('security_reports/gl-license-management-report-v2.json', dir: 'ee')) }
+    let(:expected_components) do
+      report['dependencies']
+        .find_all { |dependency| dependency['licenses'].include?(mit.spdx_identifier) }
+        .map { |dependency| dependency['name'] }
+    end
 
     before do
+      visit path
       wait_for_requests
     end
 
     it 'displays licenses detected in the most recent scan report' do
-      expect(page).to have_content(mit.name)
-    end
+      selector = "div[data-spdx-id='#{mit.spdx_identifier}'"
+      expect(page).to have_selector(selector)
 
-    it 'displays the classification for detected licenses' do
-      expect(page).to have_content(mit_policy.classification)
+      row = page.find(selector)
+      expect(row).to have_content(mit.name)
+      expect(row).to have_content(expected_components.join(' and '))
     end
   end
 end
