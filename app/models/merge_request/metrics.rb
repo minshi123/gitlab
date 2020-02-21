d8121d6cf69243178a5ae9419f45d6b411a2409e
Pavel Shutsin <pshutsin@gitlab.com>
2020-02-21 16:01:06 +0300

    Refactor analytics metrics calculation
    Code restructuring with no behavior changes

--- app/models/merge_request/metrics.rb
+++ app/models/merge_request/metrics.rb
@@ -5,4 +5,10 @@ class MergeRequest::Metrics < ApplicationRecord
   belongs_to :pipeline, class_name: 'Ci::Pipeline', foreign_key: :pipeline_id
   belongs_to :latest_closed_by, class_name: 'User'
   belongs_to :merged_by, class_name: 'User'
+
+  def review_time
+    return unless first_comment_at
+
+    (merged_at || Time.now) - first_comment_at
+  end
 end
