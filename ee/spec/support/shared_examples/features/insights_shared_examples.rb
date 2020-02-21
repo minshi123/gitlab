102a393660b6bbfe3fca9bc8df313126ef6fdaad
Mark Fletcher <mark@gitlab.com>
2020-02-21 10:51:51 +0000

    Fix Insights displaying JSON on back navigation

--- ee/spec/support/shared_examples/features/insights_shared_examples.rb
+++ ee/spec/support/shared_examples/features/insights_shared_examples.rb
@@ -3,7 +3,7 @@
 RSpec.shared_examples 'Insights page' do
   let_it_be(:user) { create(:user) }
 
-  context 'as a permitted user' do
+  describe 'as a permitted user' do
     before(:context) do
       entity.add_maintainer(user)
     end
@@ -24,27 +24,39 @@ RSpec.shared_examples 'Insights page' do
         expect(page).to have_content('Insights')
       end
 
-      context 'hash fragment navigation', :js do
-        let(:config) { entity.insights_config }
-        let(:non_default_tab_id) { config.keys.last }
-        let(:non_default_tab_title) { config[non_default_tab_id][:title] }
-        let(:hash_fragment) { "#/#{non_default_tab_id}" }
-        let(:route) { path + hash_fragment }
-
+      describe 'navigation', :js do
         before do
           visit route
-
           wait_for_requests
         end
 
-        it 'loads the correct page' do
-          page.within(".insights-container") do
-            expect(page).to have_content(non_default_tab_title)
+        describe 'hash fragment navigation' do
+          let(:config) { entity.insights_config }
+          let(:non_default_tab_id) { config.keys.last }
+          let(:non_default_tab_title) { config[non_default_tab_id][:title] }
+          let(:hash_fragment) { "#/#{non_default_tab_id}" }
+          let(:route) { path + hash_fragment }
+
+          it 'loads the correct page' do
+            page.within(".insights-container") do
+              expect(page).to have_content(non_default_tab_title)
+            end
           end
+        end
+
+        it 'displays correctly when navigating back to insights' do
+          expect(page).to have_content('Insights')
+
+          visit root_path
+
+          page.evaluate_script('window.history.back()')
+
+          expect(page).to have_gitlab_http_status(:ok)
+          expect(page).to have_content('Insights')
         end
       end
 
-      context 'when the feature flag is disabled globally' do
+      describe 'when the feature flag is disabled globally' do
         before do
           stub_feature_flags(insights: false)
         end
@@ -57,7 +69,7 @@ RSpec.shared_examples 'Insights page' do
       end
     end
 
-    context 'without correct license' do
+    describe 'without correct license' do
       before do
         stub_feature_flags(insights: { enabled: false, thing: entity })
         stub_licensed_features(insights: false)
