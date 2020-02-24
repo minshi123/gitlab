3f4003f026ab8e85fe5ae16813c69a30260e1e0a
Martin Wortschack <mwortschack@gitlab.com>
2020-02-24 12:42:56 +0100

    Add empty state screen
    - Display an empty state screen for code review analytics

--- ee/app/assets/javascripts/analytics/code_review_analytics/components/app.vue
+++ ee/app/assets/javascripts/analytics/code_review_analytics/components/app.vue
@@ -1,6 +1,6 @@
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex';
-import { GlBadge, GlLoadingIcon, GlPagination } from '@gitlab/ui';
+import { GlBadge, GlLoadingIcon, GlEmptyState, GlPagination } from '@gitlab/ui';
 import MergeRequestTable from './merge_request_table.vue';
 
 export default {
@@ -8,11 +8,20 @@ export default {
     GlBadge,
     GlLoadingIcon,
     GlPagination,
+    GlEmptyState,
     MergeRequestTable,
   },
   props: {
     projectId: {
       type: Number,
+      required: true,
+    },
+    newMergeRequestUrl: {
+      type: String,
+      required: true,
+    },
+    emptyStateSvgPath: {
+      type: String,
       required: true,
     },
   },
@@ -46,20 +55,35 @@ export default {
 
 <template>
   <div class="mt-2">
-    <div>
+    <div v-if="totalItems">
       <span class="font-weight-bold">{{ __('Merge Requests in Review') }}</span>
       <gl-badge v-show="showMrCount" pill>{{ totalItems }}</gl-badge>
     </div>
     <gl-loading-icon v-show="isLoading" size="md" class="mt-3" />
     <template v-if="!isLoading">
-      <merge-request-table />
-      <gl-pagination
-        v-model="currentPage"
-        :per-page="perPage"
-        :total-items="totalItems"
-        align="center"
-        class="w-100"
-      />
+      <gl-empty-state
+        v-if="!totalItems"
+        :title="__('You don\'t have any open merge requests')"
+        :description="
+          __(
+            'Code Review Analytics displays a table of open merge requests considered to be in code review. There are currently no merge requests in review for this project and/or filters',
+          )
+        "
+        :primary-button-text="__('New merge request')"
+        :primary-button-link="newMergeRequestUrl"
+        :svg-path="emptyStateSvgPath"
+      >
+      </gl-empty-state>
+      <template v-else>
+        <merge-request-table />
+        <gl-pagination
+          v-model="currentPage"
+          :per-page="perPage"
+          :total-items="totalItems"
+          align="center"
+          class="w-100"
+        />
+      </template>
     </template>
   </div>
 </template>
