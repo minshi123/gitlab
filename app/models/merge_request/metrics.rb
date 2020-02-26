01a3c980d336cc0aab95ff176bb267a870af4187
Pavel Shutsin <pshutsin@gitlab.com>
2020-02-26 15:50:22 +0300

    Consider first approved at in code review time

--- app/models/merge_request/metrics.rb
+++ app/models/merge_request/metrics.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 class MergeRequest::Metrics < ApplicationRecord
+  include Gitlab::Utils::StrongMemoize
+
   belongs_to :merge_request
   belongs_to :pipeline, class_name: 'Ci::Pipeline', foreign_key: :pipeline_id
   belongs_to :latest_closed_by, class_name: 'User'
   belongs_to :merged_by, class_name: 'User'
-
-  def review_time
-    return unless first_comment_at
-
-    (merged_at || Time.now) - first_comment_at
-  end
 end
+
+MergeRequest::Metrics.prepend_if_ee('EE::MergeRequest::Metrics')
