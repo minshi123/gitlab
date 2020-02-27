df503c3db3367a982d65d7a182a2b0924c8c144a
Miguel Rincon <mrincon@gitlab.com>
2020-02-27 11:44:22 +0800

    Add IEC format for digital units
    IEC recommends using binary scales with the units.
    
    Old `bytes` format is replaced with `decimalBytes`.

--- spec/frontend/lib/utils/unit_format/formatter_factory_spec.js
+++ spec/frontend/lib/utils/unit_format/formatter_factory_spec.js
@@ -2,6 +2,7 @@ import {
   numberFormatter,
   suffixFormatter,
   scaledSIFormatter,
+  scaledBinaryFormatter,
 } from '~/lib/utils/unit_format/formatter_factory';
 
 describe('unit_format/formatter_factory', () => {
@@ -94,62 +95,62 @@ describe('unit_format/formatter_factory', () => {
 
   describe('scaledSIFormatter', () => {
     describe('scaled format', () => {
-      let formatScaled;
+      let formatGibibytes;
 
       beforeEach(() => {
-        formatScaled = scaledSIFormatter('B');
+        formatGibibytes = scaledSIFormatter('B');
       });
 
       it('formats bytes', () => {
-        expect(formatScaled(12.345)).toEqual('12.345B');
-        expect(formatScaled(12.345, 0)).toEqual('12B');
-        expect(formatScaled(12.345, 1)).toEqual('12.3B');
-        expect(formatScaled(12.345, 2)).toEqual('12.35B');
+        expect(formatGibibytes(12.345)).toEqual('12.345B');
+        expect(formatGibibytes(12.345, 0)).toEqual('12B');
+        expect(formatGibibytes(12.345, 1)).toEqual('12.3B');
+        expect(formatGibibytes(12.345, 2)).toEqual('12.35B');
       });
 
-      it('formats bytes in a scale', () => {
-        expect(formatScaled(1)).toEqual('1B');
-        expect(formatScaled(10)).toEqual('10B');
-        expect(formatScaled(10 ** 2)).toEqual('100B');
-        expect(formatScaled(10 ** 3)).toEqual('1kB');
-        expect(formatScaled(10 ** 4)).toEqual('10kB');
-        expect(formatScaled(10 ** 5)).toEqual('100kB');
-        expect(formatScaled(10 ** 6)).toEqual('1MB');
-        expect(formatScaled(10 ** 7)).toEqual('10MB');
-        expect(formatScaled(10 ** 8)).toEqual('100MB');
-        expect(formatScaled(10 ** 9)).toEqual('1GB');
-        expect(formatScaled(10 ** 10)).toEqual('10GB');
-        expect(formatScaled(10 ** 11)).toEqual('100GB');
+      it('formats bytes in a decimal scale', () => {
+        expect(formatGibibytes(1)).toEqual('1B');
+        expect(formatGibibytes(10)).toEqual('10B');
+        expect(formatGibibytes(10 ** 2)).toEqual('100B');
+        expect(formatGibibytes(10 ** 3)).toEqual('1kB');
+        expect(formatGibibytes(10 ** 4)).toEqual('10kB');
+        expect(formatGibibytes(10 ** 5)).toEqual('100kB');
+        expect(formatGibibytes(10 ** 6)).toEqual('1MB');
+        expect(formatGibibytes(10 ** 7)).toEqual('10MB');
+        expect(formatGibibytes(10 ** 8)).toEqual('100MB');
+        expect(formatGibibytes(10 ** 9)).toEqual('1GB');
+        expect(formatGibibytes(10 ** 10)).toEqual('10GB');
+        expect(formatGibibytes(10 ** 11)).toEqual('100GB');
       });
     });
 
     describe('scaled format with offset', () => {
-      let formatScaled;
+      let formatGigaBytes;
 
       beforeEach(() => {
         // formats gigabytes
-        formatScaled = scaledSIFormatter('B', 3);
+        formatGigaBytes = scaledSIFormatter('B', 3);
       });
 
       it('formats floating point numbers', () => {
-        expect(formatScaled(12.345)).toEqual('12.345GB');
-        expect(formatScaled(12.345, 0)).toEqual('12GB');
-        expect(formatScaled(12.345, 1)).toEqual('12.3GB');
-        expect(formatScaled(12.345, 2)).toEqual('12.35GB');
+        expect(formatGigaBytes(12.345)).toEqual('12.345GB');
+        expect(formatGigaBytes(12.345, 0)).toEqual('12GB');
+        expect(formatGigaBytes(12.345, 1)).toEqual('12.3GB');
+        expect(formatGigaBytes(12.345, 2)).toEqual('12.35GB');
       });
 
       it('formats large numbers scaled', () => {
-        expect(formatScaled(1)).toEqual('1GB');
-        expect(formatScaled(1, 1)).toEqual('1.0GB');
-        expect(formatScaled(10)).toEqual('10GB');
-        expect(formatScaled(10 ** 2)).toEqual('100GB');
-        expect(formatScaled(10 ** 3)).toEqual('1TB');
-        expect(formatScaled(10 ** 4)).toEqual('10TB');
-        expect(formatScaled(10 ** 5)).toEqual('100TB');
-        expect(formatScaled(10 ** 6)).toEqual('1PB');
-        expect(formatScaled(10 ** 7)).toEqual('10PB');
-        expect(formatScaled(10 ** 8)).toEqual('100PB');
-        expect(formatScaled(10 ** 9)).toEqual('1EB');
+        expect(formatGigaBytes(1)).toEqual('1GB');
+        expect(formatGigaBytes(1, 1)).toEqual('1.0GB');
+        expect(formatGigaBytes(10)).toEqual('10GB');
+        expect(formatGigaBytes(10 ** 2)).toEqual('100GB');
+        expect(formatGigaBytes(10 ** 3)).toEqual('1TB');
+        expect(formatGigaBytes(10 ** 4)).toEqual('10TB');
+        expect(formatGigaBytes(10 ** 5)).toEqual('100TB');
+        expect(formatGigaBytes(10 ** 6)).toEqual('1PB');
+        expect(formatGigaBytes(10 ** 7)).toEqual('10PB');
+        expect(formatGigaBytes(10 ** 8)).toEqual('100PB');
+        expect(formatGigaBytes(10 ** 9)).toEqual('1EB');
       });
 
       it('formatting of too large numbers is not suported', () => {
@@ -159,41 +160,117 @@ describe('unit_format/formatter_factory', () => {
     });
 
     describe('scaled format with negative offset', () => {
-      let formatScaled;
+      let formatMilligrams;
 
       beforeEach(() => {
-        formatScaled = scaledSIFormatter('g', -1);
+        formatMilligrams = scaledSIFormatter('g', -1);
       });
 
       it('formats floating point numbers', () => {
-        expect(formatScaled(12.345)).toEqual('12.345mg');
-        expect(formatScaled(12.345, 0)).toEqual('12mg');
-        expect(formatScaled(12.345, 1)).toEqual('12.3mg');
-        expect(formatScaled(12.345, 2)).toEqual('12.35mg');
+        expect(formatMilligrams(1.0)).toEqual('1mg');
+        expect(formatMilligrams(12.345)).toEqual('12.345mg');
+        expect(formatMilligrams(12.345, 0)).toEqual('12mg');
+        expect(formatMilligrams(12.345, 1)).toEqual('12.3mg');
+        expect(formatMilligrams(12.345, 2)).toEqual('12.35mg');
       });
 
       it('formats large numbers scaled', () => {
-        expect(formatScaled(1)).toEqual('1mg');
-        expect(formatScaled(1, 1)).toEqual('1.0mg');
-        expect(formatScaled(10)).toEqual('10mg');
-        expect(formatScaled(10 ** 2)).toEqual('100mg');
-        expect(formatScaled(10 ** 3)).toEqual('1g');
-        expect(formatScaled(10 ** 4)).toEqual('10g');
-        expect(formatScaled(10 ** 5)).toEqual('100g');
-        expect(formatScaled(10 ** 6)).toEqual('1kg');
-        expect(formatScaled(10 ** 7)).toEqual('10kg');
-        expect(formatScaled(10 ** 8)).toEqual('100kg');
+        expect(formatMilligrams(10)).toEqual('10mg');
+        expect(formatMilligrams(10 ** 2)).toEqual('100mg');
+        expect(formatMilligrams(10 ** 3)).toEqual('1g');
+        expect(formatMilligrams(10 ** 4)).toEqual('10g');
+        expect(formatMilligrams(10 ** 5)).toEqual('100g');
+        expect(formatMilligrams(10 ** 6)).toEqual('1kg');
+        expect(formatMilligrams(10 ** 7)).toEqual('10kg');
+        expect(formatMilligrams(10 ** 8)).toEqual('100kg');
       });
 
       it('formats negative numbers scaled', () => {
-        expect(formatScaled(-12.345)).toEqual('-12.345mg');
-        expect(formatScaled(-12.345, 0)).toEqual('-12mg');
-        expect(formatScaled(-12.345, 1)).toEqual('-12.3mg');
-        expect(formatScaled(-12.345, 2)).toEqual('-12.35mg');
+        expect(formatMilligrams(-12.345)).toEqual('-12.345mg');
+        expect(formatMilligrams(-12.345, 0)).toEqual('-12mg');
+        expect(formatMilligrams(-12.345, 1)).toEqual('-12.3mg');
+        expect(formatMilligrams(-12.345, 2)).toEqual('-12.35mg');
 
-        expect(formatScaled(-10)).toEqual('-10mg');
-        expect(formatScaled(-100)).toEqual('-100mg');
-        expect(formatScaled(-(10 ** 4))).toEqual('-10g');
+        expect(formatMilligrams(-10)).toEqual('-10mg');
+        expect(formatMilligrams(-100)).toEqual('-100mg');
+        expect(formatMilligrams(-(10 ** 4))).toEqual('-10g');
+      });
+    });
+  });
+
+  describe('scaledBinaryFormatter', () => {
+    describe('scaled format', () => {
+      let formatScaledBin;
+
+      beforeEach(() => {
+        formatScaledBin = scaledBinaryFormatter('B');
+      });
+
+      it('formats bytes', () => {
+        expect(formatScaledBin(12.345)).toEqual('12.345B');
+        expect(formatScaledBin(12.345, 0)).toEqual('12B');
+        expect(formatScaledBin(12.345, 1)).toEqual('12.3B');
+        expect(formatScaledBin(12.345, 2)).toEqual('12.35B');
+      });
+
+      it('formats bytes in a binary scale', () => {
+        expect(formatScaledBin(1)).toEqual('1B');
+        expect(formatScaledBin(10)).toEqual('10B');
+        expect(formatScaledBin(100)).toEqual('100B');
+        expect(formatScaledBin(1000)).toEqual('1,000B');
+        expect(formatScaledBin(10000)).toEqual('9.766KiB');
+
+        expect(formatScaledBin(1 * 1024)).toEqual('1KiB');
+        expect(formatScaledBin(10 * 1024)).toEqual('10KiB');
+        expect(formatScaledBin(100 * 1024)).toEqual('100KiB');
+
+        expect(formatScaledBin(1 * 1024 ** 2)).toEqual('1MiB');
+        expect(formatScaledBin(10 * 1024 ** 2)).toEqual('10MiB');
+        expect(formatScaledBin(100 * 1024 ** 2)).toEqual('100MiB');
+
+        expect(formatScaledBin(1 * 1024 ** 3)).toEqual('1GiB');
+        expect(formatScaledBin(10 * 1024 ** 3)).toEqual('10GiB');
+        expect(formatScaledBin(100 * 1024 ** 3)).toEqual('100GiB');
+      });
+    });
+
+    describe('scaled format with offset', () => {
+      let formatGibibytes;
+
+      beforeEach(() => {
+        formatGibibytes = scaledBinaryFormatter('B', 3);
+      });
+
+      it('formats floating point numbers', () => {
+        expect(formatGibibytes(12.888)).toEqual('12.888GiB');
+        expect(formatGibibytes(12.888, 0)).toEqual('13GiB');
+        expect(formatGibibytes(12.888, 1)).toEqual('12.9GiB');
+        expect(formatGibibytes(12.888, 2)).toEqual('12.89GiB');
+      });
+
+      it('formats large numbers scaled', () => {
+        expect(formatGibibytes(1)).toEqual('1GiB');
+        expect(formatGibibytes(10)).toEqual('10GiB');
+        expect(formatGibibytes(100)).toEqual('100GiB');
+        expect(formatGibibytes(1000)).toEqual('1,000GiB');
+
+
+        expect(formatGibibytes(1 * 1024)).toEqual('1TiB');
+        expect(formatGibibytes(10 * 1024)).toEqual('10TiB');
+        expect(formatGibibytes(100 * 1024)).toEqual('100TiB');
+
+        expect(formatGibibytes(1 * 1024 ** 2)).toEqual('1PiB');
+        expect(formatGibibytes(10 * 1024 ** 2)).toEqual('10PiB');
+        expect(formatGibibytes(100 * 1024 ** 2)).toEqual('100PiB');
+
+        expect(formatGibibytes(1 * 1024 ** 3)).toEqual('1EiB');
+        expect(formatGibibytes(10 * 1024 ** 3)).toEqual('10EiB');
+        expect(formatGibibytes(100 * 1024 ** 3)).toEqual('100EiB');
+      });
+
+      it('formatting of too large numbers is not suported', () => {
+        // formatting YB is out of range
+        expect(() => scaledBinaryFormatter('B', 9)).toThrow();
       });
     });
   });
--- spec/frontend/lib/utils/unit_format/index_spec.js
+++ spec/frontend/lib/utils/unit_format/index_spec.js
@@ -54,33 +54,33 @@ describe('unit_format', () => {
       expect(getFormatter(SUPPORTED_FORMATS.seconds)(1)).toEqual('1s');
     });
 
-    it('miliseconds', () => {
-      const formatMiliseconds = getFormatter(SUPPORTED_FORMATS.miliseconds);
+    it('milliseconds', () => {
+      const formatMilliseconds = getFormatter(SUPPORTED_FORMATS.milliseconds);
 
-      expect(formatMiliseconds(1)).toEqual('1ms');
-      expect(formatMiliseconds(100)).toEqual('100ms');
-      expect(formatMiliseconds(1000)).toEqual('1,000ms');
-      expect(formatMiliseconds(10000)).toEqual('10,000ms');
-      expect(formatMiliseconds(1000000)).toEqual('1,000,000ms');
+      expect(formatMilliseconds(1)).toEqual('1ms');
+      expect(formatMilliseconds(100)).toEqual('100ms');
+      expect(formatMilliseconds(1000)).toEqual('1,000ms');
+      expect(formatMilliseconds(10000)).toEqual('10,000ms');
+      expect(formatMilliseconds(1000000)).toEqual('1,000,000ms');
     });
 
-    it('bytes', () => {
-      const formatBytes = getFormatter(SUPPORTED_FORMATS.bytes);
+    it('decimalBytes', () => {
+      const formatDecimalBytes = getFormatter(SUPPORTED_FORMATS.decimalBytes);
 
-      expect(formatBytes(1)).toEqual('1B');
-      expect(formatBytes(1, 1)).toEqual('1.0B');
+      expect(formatDecimalBytes(1)).toEqual('1B');
+      expect(formatDecimalBytes(1, 1)).toEqual('1.0B');
 
-      expect(formatBytes(10)).toEqual('10B');
-      expect(formatBytes(10 ** 2)).toEqual('100B');
-      expect(formatBytes(10 ** 3)).toEqual('1kB');
-      expect(formatBytes(10 ** 4)).toEqual('10kB');
-      expect(formatBytes(10 ** 5)).toEqual('100kB');
-      expect(formatBytes(10 ** 6)).toEqual('1MB');
-      expect(formatBytes(10 ** 7)).toEqual('10MB');
-      expect(formatBytes(10 ** 8)).toEqual('100MB');
-      expect(formatBytes(10 ** 9)).toEqual('1GB');
-      expect(formatBytes(10 ** 10)).toEqual('10GB');
-      expect(formatBytes(10 ** 11)).toEqual('100GB');
+      expect(formatDecimalBytes(10)).toEqual('10B');
+      expect(formatDecimalBytes(10 ** 2)).toEqual('100B');
+      expect(formatDecimalBytes(10 ** 3)).toEqual('1kB');
+      expect(formatDecimalBytes(10 ** 4)).toEqual('10kB');
+      expect(formatDecimalBytes(10 ** 5)).toEqual('100kB');
+      expect(formatDecimalBytes(10 ** 6)).toEqual('1MB');
+      expect(formatDecimalBytes(10 ** 7)).toEqual('10MB');
+      expect(formatDecimalBytes(10 ** 8)).toEqual('100MB');
+      expect(formatDecimalBytes(10 ** 9)).toEqual('1GB');
+      expect(formatDecimalBytes(10 ** 10)).toEqual('10GB');
+      expect(formatDecimalBytes(10 ** 11)).toEqual('100GB');
     });
 
     it('kilobytes', () => {
@@ -106,6 +106,46 @@ describe('unit_format', () => {
     it('petabytes', () => {
       expect(getFormatter(SUPPORTED_FORMATS.petabytes)(1)).toEqual('1PB');
       expect(getFormatter(SUPPORTED_FORMATS.petabytes)(1, 1)).toEqual('1.0PB');
+    });
+
+    it('bytes', () => {
+      const formatBytes = getFormatter(SUPPORTED_FORMATS.bytes);
+
+      expect(formatBytes(1)).toEqual('1B');
+      expect(formatBytes(1, 1)).toEqual('1.0B');
+
+      expect(formatBytes(10)).toEqual('10B');
+      expect(formatBytes(100)).toEqual('100B');
+      expect(formatBytes(1000)).toEqual('1,000B');
+
+      expect(formatBytes(1 * 1024)).toEqual('1KiB');
+      expect(formatBytes(1 * 1024 ** 2)).toEqual('1MiB');
+      expect(formatBytes(1 * 1024 ** 3)).toEqual('1GiB');
+    });
+
+    it('kibibytes', () => {
+      expect(getFormatter(SUPPORTED_FORMATS.kibibytes)(1)).toEqual('1KiB');
+      expect(getFormatter(SUPPORTED_FORMATS.kibibytes)(1, 1)).toEqual('1.0KiB');
+    });
+
+    it('mebibytes', () => {
+      expect(getFormatter(SUPPORTED_FORMATS.mebibytes)(1)).toEqual('1MiB');
+      expect(getFormatter(SUPPORTED_FORMATS.mebibytes)(1, 1)).toEqual('1.0MiB');
+    });
+
+    it('gibibytes', () => {
+      expect(getFormatter(SUPPORTED_FORMATS.gibibytes)(1)).toEqual('1GiB');
+      expect(getFormatter(SUPPORTED_FORMATS.gibibytes)(1, 1)).toEqual('1.0GiB');
+    });
+
+    it('tebibytes', () => {
+      expect(getFormatter(SUPPORTED_FORMATS.tebibytes)(1)).toEqual('1TiB');
+      expect(getFormatter(SUPPORTED_FORMATS.tebibytes)(1, 1)).toEqual('1.0TiB');
+    });
+
+    it('pebibytes', () => {
+      expect(getFormatter(SUPPORTED_FORMATS.pebibytes)(1)).toEqual('1PiB');
+      expect(getFormatter(SUPPORTED_FORMATS.pebibytes)(1, 1)).toEqual('1.0PiB');
     });
   });
 
