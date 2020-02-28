03368000090b0f24433994bd3e53a99416ab983f
Marius Bobin <mbobin@gitlab.com>
2020-02-28 17:23:15 +0200

    Add license check to GH pull requests service

--- ee/app/services/process_github_pull_request_event_service.rb
+++ ee/app/services/process_github_pull_request_event_service.rb
@@ -12,6 +12,7 @@ class ProcessGithubPullRequestEventService < ::BaseService
 
   def execute(webhook_params)
     return unless project.mirror?
+    return unless project.feature_available?(:ci_cd_projects, current_user)
 
     params = params_from_webhook(webhook_params)
     return unless params[:status]
