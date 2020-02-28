c918879a30d4a579f3ba1998d5cbcf7f55ada33c
Małgorzata Ksionek <mksionek@gitlab.com>
2020-02-28 14:08:03 +0100

    Add checking project  members membership in group managed account

---
+++ ee/lib/gitlab/auth/group_saml/gma_membership_enforcer.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module Auth
+    module GroupSaml
+      class GmaMembershipEnforcer
+        def initialize(project)
+          @project = project
+        end
+
+        def can_add_user?(user)
+          return true unless ::Feature.enabled?(:group_managed_accounts, root_group)
+          return true unless root_group&.enforced_group_managed_accounts?
+
+          root_group == user.managing_group
+        end
+
+        private
+
+        def root_group
+          @root_group ||= @project&.root_ancestor
+        end
+      end
+    end
+  end
+end
--- ee/spec/models/project_member_spec.rb
+++ ee/spec/models/project_member_spec.rb
@@ -7,4 +7,41 @@ describe ProjectMember do
   it_behaves_like 'member validations' do
     let(:entity) { create(:project, group: group)}
   end
+
+  context 'validates GMA enforcement' do
+    let(:group) { create(:group_with_managed_accounts, :private) }
+    let(:user) { create(:user, :group_managed, managing_group: group) }
+    let(:entity) { create(:project, namespace: group)}
+
+    before do
+      stub_feature_flags(group_managed_accounts: true)
+    end
+
+    context 'enforced group managed account enabled' do
+      before do
+        allow_any_instance_of(SamlProvider).to receive(:enforced_group_managed_accounts?).and_return(true)
+      end
+
+      it 'allows adding the project member' do
+        member = described_class.add_user(entity, user, Member::DEVELOPER)
+
+        expect(member).to be_valid
+      end
+
+      it 'does not add the the project member' do
+        member = described_class.add_user(entity, create(:user), Member::DEVELOPER)
+
+        expect(member).not_to be_valid
+        expect(member.errors.messages[:user]).to eq(['is not in the group enforcing Group Managed Account'])
+      end
+    end
+
+    context 'enforced group managed account disabled' do
+      it 'allows adding the group member' do
+        member = described_class.add_user(entity, create(:user), Member::DEVELOPER)
+
+        expect(member).to be_valid
+      end
+    end
+  end
 end
