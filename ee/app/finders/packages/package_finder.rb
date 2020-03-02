0c565a8c2ebb9e84e97c68b623caa3ccccde7f0c
Nick Kipling <nkipling@gitlab.com>
2020-03-02 17:03:40 +0000

    Reverted attr_reader changes in package_finder

--- ee/app/finders/packages/package_finder.rb
+++ ee/app/finders/packages/package_finder.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 module Packages
   class PackageFinder
-    attr_reader :package_id, :project
-
     def initialize(project, package_id)
       @project = project
       @package_id = package_id
     end
 
     def execute
-      project
+      @project
         .packages
         .processed
-        .find(package_id)
+        .find(@package_id)
     end
   end
 end
