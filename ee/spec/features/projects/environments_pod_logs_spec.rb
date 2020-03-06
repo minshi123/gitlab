c0e2eb97c991454e5c17fe0bb739694358a92964
Miguel Rincon <mrincon@gitlab.com>
2020-03-06 11:29:10 +0800

    Replace form header with dropdown header in filter
    To make better use of space the form group header is removed and
    dropdown header is enabled in two log filter dropdowns.
    
    Height of filter box is adjusted to match and "selected" icons are
    added.

--- ee/spec/features/projects/environments_pod_logs_spec.rb
+++ ee/spec/features/projects/environments_pod_logs_spec.rb
@@ -47,6 +47,8 @@ describe 'Environment > Pod Logs', :js do
 
       wait_for_requests
 
+      expect(page).to have_content("Dec 13 14:04:22.123Z | kube-pod | Log 1 Dec 13 14:04:23.123Z | kube-pod | Log 2 Dec 13 14:04:24.123Z | kube-pod | Log 3")
+
       page.within('.js-pods-dropdown') do
         find(".dropdown-menu-toggle:not([disabled])").click
 
@@ -57,7 +59,6 @@ describe 'Environment > Pod Logs', :js do
           expect(item.text).to eq(pod_names[i])
         end
       end
-      expect(page).to have_content("Dec 13 14:04:22.123Z | kube-pod | Log 1 Dec 13 14:04:23.123Z | kube-pod | Log 2 Dec 13 14:04:24.123Z | kube-pod | Log 3")
     end
   end
 end
