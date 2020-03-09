1388c97f7ae13e7fa36e488b13609b5630d5d111
Alan Paruszewski <mparuszewski@gitlab.com>
2020-03-09 11:30:31 +0100

    Add CSV export for Security Reports

---
+++ ee/app/services/vulnerabilities/export_csv_service.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Vulnerabilities
+  class ExportCsvService
+    def initialize(findings_relation)
+      @findings = findings_relation
+    end
+
+    def csv_data
+      csv_builder.render
+    end
+
+    # rubocop: disable CodeReuse/ActiveRecord
+    def csv_builder
+      @csv_builder ||= CsvBuilder.new(@findings.preload(:scanner), header_to_value_hash)
+    end
+    # rubocop: enable CodeReuse/ActiveRecord
+
+    private
+
+    def header_to_value_hash
+      {
+        'Scanner Type' => 'report_type',
+        'Scanner Name' => -> (finding) { finding.scanner.name },
+        'Vulnerability' => 'name',
+        'Details' => 'description',
+        'Additional Info' => -> (finding) { finding.metadata['message'] },
+        'Severity' => 'severity',
+        'CVE' => -> (finding) { finding.metadata['cve'] }
+      }
+    end
+  end
+end
