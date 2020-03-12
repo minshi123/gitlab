4e53081bfd731d74bd870b4689bd36b692ee831a
Qingyu Zhao <qzhao@gitlab.com>
2020-03-12 15:01:49 +1100

    Address part of the review comments

--- lib/gitlab/import_export/project/tree_restorer.rb
+++ lib/gitlab/import_export/project/tree_restorer.rb
@@ -17,11 +17,7 @@ module Gitlab
         def restore
           @relation_reader = ImportExport::JSON::LegacyReader.new(File.join(shared.export_path, 'project.json'))
 
-          @project_members = []
-
-          @relation_reader.consume_relation('project_members') do |project_member|
-            @project_members << project_member
-          end
+          @project_members = @relation_reader.delete('project_members')
 
           if relation_tree_restorer.restore
             import_failure_service.with_retry(action: 'set_latest_merge_request_diff_ids!') do
