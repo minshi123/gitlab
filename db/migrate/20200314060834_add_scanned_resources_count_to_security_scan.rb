e56c117588c2dada9f4f46272d340a7589bf5f9b
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2020-03-14 18:01:18 +1100

    Use 1.9 hash syntax

--- db/migrate/20200314060834_add_scanned_resources_count_to_security_scan.rb
+++ db/migrate/20200314060834_add_scanned_resources_count_to_security_scan.rb
@@ -3,6 +3,6 @@ class AddScannedResourcesCountToSecurityScan < ActiveRecord::Migration[6.0]
   DOWNTIME = false
 
   def change
-    add_column :security_scans, :scanned_resources_count, :integer, :null => false, :default => 0
+    add_column :security_scans, :scanned_resources_count, :integer, null: false, default: 0
   end
 end
