3ab5476765a03c37f002cb9624a2483443120e1e
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2020-03-14 18:32:38 +1100

    Use add_column_with_default_value

--- db/migrate/20200314060834_add_scanned_resources_count_to_security_scan.rb
+++ db/migrate/20200314060834_add_scanned_resources_count_to_security_scan.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 class AddScannedResourcesCountToSecurityScan < ActiveRecord::Migration[6.0]
+  include Gitlab::Database::MigrationHelpers
+
   DOWNTIME = false
 
-  def change
-    add_column :security_scans, :scanned_resources_count, :integer, null: false, default: 0
+  disable_ddl_transaction!
+
+  def up
+    add_column_with_default(:security_scans, :scanned_resources_count, :integer, default: 0, allow_null: false)
+  end
+
+  def down
+    remove_column :security_scans, :scanned_resources_count
   end
 end
