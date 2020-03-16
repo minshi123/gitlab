89c64140ac5b2ca4783f37b26080aa10278c8e02
alinamihaila <amihaila@gitlab.com>
2020-03-16 14:41:50 +0200

    Test change in usage data file

--- lib/gitlab/usage_data.rb
+++ lib/gitlab/usage_data.rb
@@ -5,6 +5,7 @@ module Gitlab
     APPROXIMATE_COUNT_MODELS = [Label, MergeRequest, Note, Todo].freeze
     BATCH_SIZE = 100
 
+    # A test change
     class << self
       def data(force_refresh: false)
         Rails.cache.fetch('usage_data', force: force_refresh, expires_in: 2.weeks) do
