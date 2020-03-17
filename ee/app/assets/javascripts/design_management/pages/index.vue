63a99876c9ee0648dd10ae54f5add809b913196c
ntepluhina <ntepluhina@gitlab.com>
2020-03-17 16:25:11 +0200

    Added errors-as-data handling

--- ee/app/assets/javascripts/design_management/pages/index.vue
+++ ee/app/assets/javascripts/design_management/pages/index.vue
@@ -158,6 +158,12 @@ export default {
         createFlash(skippedWarningMessage, 'warning');
       }
 
+      // Handling errors-as-data
+      const errors = res?.data?.designManagementUpload?.errors || [];
+      if (errors.length > 0) {
+        createFlash(errors[0]);
+      }
+
       // if this upload resulted in a new version being created, redirect user to the latest version
       if (!this.isLatestVersion) {
         this.$router.push({ name: DESIGNS_ROUTE_NAME });
