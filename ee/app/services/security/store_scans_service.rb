5125851243c42abbae874ddc3dd4c9b98316b17f
Craig Smith <5344211-craigmsmith@users.noreply.gitlab.com>
2020-03-17 12:38:40 +1100

    Fix formatting

--- ee/app/services/security/store_scans_service.rb
+++ ee/app/services/security/store_scans_service.rb
@@ -14,7 +14,7 @@ module Security
       ActiveRecord::Base.transaction do
         security_reports.each do |job_artifact|
           report = ::Gitlab::Ci::Reports::Security::Report.new(job_artifact.file_type, @build.pipeline.sha, job_artifact.created_at)
-          ::Gitlab::Ci::Parsers.fabricate!(job_artifact.file_type).parse!(job_artifact.blob,report)
+          ::Gitlab::Ci::Parsers.fabricate!(job_artifact.file_type).parse!(job_artifact.blob, report)
 
           Security::Scan.safe_find_or_create_by!(
             build: @build,
