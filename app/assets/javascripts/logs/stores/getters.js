0eb4e23a920ff0f1198dd8e22974bfa334aac389
Miguel Rincon <mrincon@gitlab.com>
2020-03-18 17:28:00 +0800

    Implement separate advanced log filters
    Split current function between simple and advanced

--- app/assets/javascripts/logs/stores/getters.js
+++ app/assets/javascripts/logs/stores/getters.js
@@ -5,5 +5,12 @@ const mapTrace = ({ timestamp = null, pod = '', message = '' }) =>
 
 export const trace = state => state.logs.lines.map(mapTrace).join('\n');
 
+export const showAdvancedFilters = state => {
+  const environment = state.environments.options.find(
+    ({ name }) => name === state.environments.current,
+  );
+  return environment && environment.enable_advanced_logs_querying;
+};
+
 // prevent babel-plugin-rewire from generating an invalid default during karma tests
 export default () => {};
