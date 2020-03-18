0a7f6b4d1593ae56181a001f6a075579a4297ef0
Alexander Turinske <aturinske@gitlab.com>
2020-03-18 14:18:11 -0600

    Add occurrence information to vulnerability
    - original the vulnerability didn't have the description and name
    - get that information from the occurrence and append it to the
      vulnerability

--- ee/app/helpers/vulnerabilities_helper.rb
+++ ee/app/helpers/vulnerabilities_helper.rb
@@ -8,7 +8,8 @@ module VulnerabilitiesHelper
       vulnerability_json: vulnerability.to_json,
       project_fingerprint: vulnerability.finding.project_fingerprint,
       create_issue_url: create_vulnerability_feedback_issue_path(vulnerability.finding.project),
-      pipeline_json: vulnerability_pipeline_data(pipeline).to_json
+      pipeline_json: vulnerability_pipeline_data(pipeline).to_json,
+      occurrence_json: vulnerability.finding.to_json
     }
   end
 
