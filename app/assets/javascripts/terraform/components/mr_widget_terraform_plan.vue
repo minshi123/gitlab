c1420c9ddc1b9f8a16546d5db6d5410c2309b777
Emily Ring <ering@gitlab.com>
2020-03-20 12:55:25 -0400

    Added responsive styles

--- app/assets/javascripts/terraform/components/mr_widget_terraform_plan.vue
+++ app/assets/javascripts/terraform/components/mr_widget_terraform_plan.vue
@@ -1,37 +1,81 @@
 <script>
+import { GlIcon } from '@gitlab/ui';
+import { escape } from 'lodash';
+import { __, sprintf } from '~/locale';
 import ciIcon from '../../vue_shared/components/ci_icon.vue';
+import MrWidgetIcon from '../../vue_merge_request_widget/components/mr_widget_icon.vue';
 
 export default {
   name: 'MRWidgetTerraformPlan',
   components: {
     ciIcon,
+    GlIcon,
+    MrWidgetIcon,
+  },
+  data() {
+    return {
+      addNum: 1,
+      changeNum: 0,
+      deleteNum: 2
+    }
   },
   computed: {
+    linkText() {
+      return __('View full log');
+    },
     iconStatusObj() {
       return {
         group: 'warning',
         icon: 'status_warning',
       };
     },
+    terraformChangesText() {
+      return sprintf(
+        __('%{addNum} to add, %{changeNum} to change, %{deleteNum} to delete'),
+        {
+          addNum: `<strong>${escape(this.addNum)}</strong>`,
+          changeNum: `<strong>${escape(this.changeNum)}</strong>`,
+          deleteNum: `<strong>${escape(this.deleteNum)}</strong>`,
+        },
+        false
+      );
+    },
+    terraformPlanText() {
+      return __('Terraform plan output the following resources:');
+    }
   }
 }
 </script>
 <template>
-  <div class="mr-widget-body media">
-    <ci-icon :status="iconStatusObj" :size="24" />
+  <section class="mr-widget-section">
+    <div class="mr-widget-body media d-flex flex-row">
+      <span class="append-right-default align-self-start align-self-lg-center">
+        <ci-icon :status="iconStatusObj" :size="24" />
+      </span>
 
-    <div class="media-body space-children">
-      <p>Terraform plan output the following resources:</p>
-      <p>
-        <strong>1</strong> to add,
-        <strong>0</strong> to change,
-        <strong>2</strong> to delete
-      </p>
+      <div class="d-flex flex-fill flex-column flex-md-row">
+        <div class="terraform-mr-plan-text normal d-flex flex-column flex-lg-row">
+          <p class="m-0 pr-1"> {{ terraformPlanText }}</p>
+          <p
+            class="m-0"
+            v-html="terraformChangesText"
+          />
+        </div>
+
+        <div class="terraform-mr-plan-actions">
+          <a
+            href="#"
+            target="_blank"
+            data-track-event="click_terraform_mr_plan_button"
+            data-track-label="mr_widget_terraform_mr_plan_button"
+            data-track-property="terraform_mr_plan_button"
+            class="btn btn-sm"
+          >
+            {{ linkText }}
+            <gl-icon name="external-link" />
+          </a>
+        </div>
+      </div>
     </div>
-    <button
-      class="btn btn-default btn-sm"
-    >
-      View Full Log
-    </button>
-  </div>
+  </section>
 </template>
