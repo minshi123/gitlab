1e57db6dd8fd74d9bec488f94ca78a0710b0a806
Justin Ho <hduong@gitlab.com>
2020-03-27 18:30:12 +0700

    Add independant active toggle component

---
+++ app/assets/javascripts/integrations/edit/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import { parseBoolean } from '~/lib/utils/common_utils';
+import ActiveToggle from './components/active_toggle.vue';
+
+export default el => {
+  if (!el) {
+    return null;
+  }
+
+  const { showActive: showActiveStr, activated: activatedStr, disabled: disabledStr } = el.dataset;
+  console.log(el.dataset);
+  console.log(showActiveStr);
+  const showActive = parseBoolean(showActiveStr);
+  const activated = parseBoolean(activatedStr);
+  const disabled = parseBoolean(disabledStr);
+
+  return new Vue({
+    el,
+    render(createElement) {
+      return createElement(ActiveToggle, {
+        props: {
+          showActive,
+          initialActivated: activated,
+          disabled,
+        }
+      });
+    },
+  });
+};
