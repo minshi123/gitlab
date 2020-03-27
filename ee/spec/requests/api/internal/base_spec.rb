7a2ab7221adbd230fdf8f09cd891a32d1148fc2a
Ash McKenzie <amckenzie@gitlab.com>
2020-03-27 15:41:01 +1100

    Simplify Geo proxying/redirecting messages
    In the future we'll be supporting not only push
    but also clone/pull.

--- ee/spec/requests/api/internal/base_spec.rb
+++ ee/spec/requests/api/internal/base_spec.rb
@@ -101,8 +101,8 @@ describe API::Internal::Base do
 
       it 'includes a message advising a redirection occurred' do
         redirect_message = <<~STR
-        You're pushing to a Geo secondary! We'll help you by redirecting this
-        request to the primary:
+        You're interacting with a Geo secondary! We'll help you by sending
+        this request to the primary:
 
           http://primary.example.com/#{project.full_path}.git
         STR
