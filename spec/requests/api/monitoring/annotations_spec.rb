86ea08e71cee88a6e019a47ac820cb40281d831d
Kirstie Cook <kcook@gitlab.com>
2020-03-31 17:33:33 -0500

    Start fleshing out specs

--- spec/requests/api/monitoring/annotations_spec.rb
+++ spec/requests/api/monitoring/annotations_spec.rb
@@ -4,4 +4,20 @@ require 'spec_helper'
 
 describe API::Monitoring::Annotations do
   let_it_be(:user) { create(:user) }
+  let_it_be(:project) { create(:project, :private, :repository, namespace: user.namespace) }
+  let_it_be(:environment) { create(:environment, project: project) }
+
+  describe 'POST /environments/:environment_id/metrics_dashboard/annotations' do
+    context 'with correct permissions' do
+      it '' do
+        expect(true).to eq(true)
+      end
+    end
+
+    context 'without correct permissions' do
+      it '' do
+        expect(true).to eq(true)
+      end
+    end
+  end
 end
