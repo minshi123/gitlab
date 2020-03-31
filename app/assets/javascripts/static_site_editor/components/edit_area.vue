71fa4283547c6ef42ef0bfc5703ae642b9a4a605
Enrique Alcantara <ealcantara@gitlab.com>
2020-03-31 15:44:16 -0400

    Bind edit area to page content state
    Display a text edit area in the static site editor
    and display  the page content in it

---
+++ app/assets/javascripts/static_site_editor/components/edit_area.vue
@@ -0,0 +1,18 @@
+<script>
+import { GlTextarea } from '@gitlab/ui';
+
+export default {
+  components: {
+    GlTextarea,
+  },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<template>
+  <gl-textarea :value="value" v-on="$listeners" />
+</template>
--- app/assets/javascripts/static_site_editor/components/static_site_editor.vue
+++ app/assets/javascripts/static_site_editor/components/static_site_editor.vue
@@ -1,3 +1,18 @@
+<script>
+import { mapState } from 'vuex';
+import EditArea from './edit_area.vue';
+
+export default {
+  components: {
+    EditArea
+  },
+  computed: {
+    ...mapState(['content', 'isContentLoaded']),
+  }
+}
+</script>
 <template>
-  <div class="static-site-editor"></div>
+  <div class="static-site-editor">
+    <edit-area v-if="isContentLoaded" :value="content" />
+  </div>
 </template>
