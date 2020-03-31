559c0f5f821a2dacbec0c674c1e623870e57908b
Enrique Alcantara <ealcantara@gitlab.com>
2020-03-31 15:44:16 -0400

    Bind edit area to page content state
    Display a text edit area in the static site editor
    and display  the page content in it

--- app/assets/javascripts/static_site_editor/components/static_site_editor.vue
+++ app/assets/javascripts/static_site_editor/components/static_site_editor.vue
@@ -1,3 +1,18 @@
+<script>
+import { mapState } from 'vuex';
+import EditArea from './edit_area.vue';
+
+export default {
+  components: {
+    EditArea
+  },
+  computed: {
+    ...mapState(['content', 'isContentLoaded']),
+  }
+}
+</script>
 <template>
-  <div></div>
+  <div>
+    <edit-area v-if="isContentLoaded" :value="content" />
+  </div>
 </template>
---
+++ spec/frontend/static_site_editor/components/static_site_editor_spec.js
@@ -0,0 +1,60 @@
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+
+import createState from '~/static_site_editor/store/state';
+
+import StaticSiteEditor from '~/static_site_editor/components/static_site_editor.vue';
+import EditArea from '~/static_site_editor/components/edit_area.vue';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe('StaticSiteEditor', () => {
+  let wrapper;
+  let store;
+
+  const buildStore = (initialState = {}) =>
+    new Vuex.Store({
+      state: createState(initialState),
+    });
+
+  const buildWrapper = () =>
+    shallowMount(StaticSiteEditor, {
+      localVue,
+      store,
+    });
+  const findEditArea = () => wrapper.find(EditArea);
+
+  beforeEach(() => {
+    store = buildStore();
+    wrapper = buildWrapper();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  describe('when content is not loaded', () => {
+    it('does not render edit area', () => {
+      expect(findEditArea().exists()).toBe(false);
+    });
+  });
+
+  describe('when content is loaded', () => {
+    const content = 'edit area content';
+
+    beforeEach(() => {
+      store = buildStore({ content, isContentLoaded: true });
+      wrapper = buildWrapper();
+    });
+
+    it('renders the edit area', () => {
+      expect(findEditArea().exists()).toBe(true);
+    });
+
+    it('passes page content to edit area', () => {
+      expect(findEditArea().props('value')).toBe(content);
+    });
+  });
+});
