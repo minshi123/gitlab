038228617a3b3d9eca5a3fadb4068e4f914e36cd
Samantha Ming <sming@gitlab.com>
2020-03-31 22:27:08 -0700

    Clean up

--- ee/app/assets/javascripts/projects/commits/components/author_select.vue
+++ ee/app/assets/javascripts/projects/commits/components/author_select.vue
@@ -1,56 +1,32 @@
 <script>
-import $ from 'jquery';
-import 'select2/select2';
-import _ from 'underscore';
-import Api from 'ee/api';
 import { __ } from '~/locale';
-import Select2Select from '~/vue_shared/components/select2_select.vue';
 import {
   GlNewDropdown,
   GlNewDropdownHeader,
   GlNewDropdownItem,
   GlSearchBoxByType,
   GlNewDropdownDivider,
-  GlAvatar,
 } from '@gitlab/ui';
 
-const SORT_OPTIONS = [
-  { key: 'DESC', text: __('Newest first'), cls: 'js-newest-first' },
-  { key: 'ASC', text: __('Oldest first'), cls: 'js-oldest-first' },
-];
-
 export default {
-  SORT_OPTIONS,
   name: 'AuthorSelect',
   components: {
-    Select2Select,
     GlNewDropdown,
     GlNewDropdownHeader,
     GlNewDropdownItem,
     GlSearchBoxByType,
     GlNewDropdownDivider,
-    GlAvatar,
   },
   data() {
     return {
       username: 'hi',
       searchTerm: '',
-      allList: ['one', 'two', 'three'],
-      avatars: [{}],
+      avatars: ['one', 'two', 'three'],
     };
   },
   computed: {
-    select2Options() {
-      return {
-        data: this.authorOptions,
-        containerCssClass: '',
-      };
-    },
-    authorOptions() {
-      return [{ id: 1, text: __('one') }, { id: 2, text: __('two') }, { id: 3, text: __('three') }];
-    },
-    filteredList() {
-      return this.allList.filter(item =>
+    filteredAvatars() {
+      return this.avatars.filter(item =>
         item.toLowerCase().includes(this.searchTerm.toLowerCase()),
       );
     },
@@ -77,28 +53,13 @@ export default {
       <gl-new-dropdown-divider />
       <!-- avatar-url="https://secure.gravatar.com/avatar/78b060780d36f51a6763ac9831a4f022?s=180&d=identicon"
         secondary-text="@sytses" -->
-      <gl-new-dropdown-item v-for="item in filteredList" :key="item" :active="item === username">
-        {{ item }}
+      <gl-new-dropdown-item
+        v-for="avatar in filteredAvatars"
+        :key="avatar"
+        :active="avatar === username"
+      >
+        {{ avatar }}
       </gl-new-dropdown-item>
     </gl-new-dropdown>
-
-    <!-- <select2-select v-model="username" :options="select2Options" /> -->
-    <!-- <div class="dropdown">
-      <button class="btn btn-sm js-dropdown-text" data-toggle="dropdown" aria-expanded="false">
-        hello there
-        <gl-icon name="chevron-down" />
-      </button>
-      <div ref="dropdownMenu" class="dropdown-menu dropdown-menu-selectable dropdown-menu-right">
-        <div class="dropdown-content">
-          <ul>
-            <li v-for="{ text, key, cls } in $options.SORT_OPTIONS" :key="key">
-              <button type="button">
-                {{ text }}
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
