80936dd7b285589fa2741834a2e9790c3cbe5e3d
derek-knox <dknox@gitlab.com>
2020-04-01 09:24:48 -0700

    Apply feedback, use s__ locale global and namespace strings

--- app/assets/javascripts/static_site_editor/components/saved_changes_message.vue
+++ app/assets/javascripts/static_site_editor/components/saved_changes_message.vue
@@ -1,5 +1,4 @@
 <script>
-import { __ } from '~/locale';
 import { GlLink, GlNewButton } from '@gitlab/ui';
 
 export default {
@@ -11,27 +10,26 @@ export default {
     links: { type: Object, required: true },
     returnUrl: { type: String, required: true },
   },
-  methods: {
-    translate: __,
-  },
 };
 </script>
 
 <template>
   <div>
     <div>
-      <h3>{{ translate('Success!') }}</h3>
+      <h3>{{ s__('StaticSiteEditor|Success!') }}</h3>
       <p>
         {{
-          translate(
-            "Your changes have been submitted and a merge request has been created. The changes won't be visible on the site until the merge request has been accepted.",
+          s__(
+            "StaticSiteEditor|Your changes have been submitted and a merge request has been created. The changes won't be visible on the site until the merge request has been accepted.",
           )
         }}
       </p>
       <div>
-        <gl-new-button :href="returnUrl">{{ translate('Return to site') }}</gl-new-button>
+        <gl-new-button :href="returnUrl">{{
+          s__('StaticSiteEditor|Return to site')
+        }}</gl-new-button>
         <gl-new-button :href="links.mergeRequest.url" variant="info">{{
-          translate('View merge request')
+          s__('StaticSiteEditor|View merge request')
         }}</gl-new-button>
       </div>
     </div>
@@ -39,18 +37,18 @@ export default {
     <hr />
 
     <div>
-      <h4>{{ translate('Summary of changes') }}</h4>
+      <h4>{{ s__('StaticSiteEditor|Summary of changes') }}</h4>
       <ul>
         <li>
-          {{ translate('A new branch was created:') }}
+          {{ s__('StaticSiteEditor|A new branch was created:') }}
           <gl-link :href="links.branch.url">{{ links.branch.label }}</gl-link>
         </li>
         <li>
-          {{ translate('Your changes were committed to it:') }}
+          {{ s__('StaticSiteEditor|Your changes were committed to it:') }}
           <gl-link :href="links.commit.url">{{ links.commit.label }}</gl-link>
         </li>
         <li>
-          {{ translate('A merge request was created:') }}
+          {{ s__('StaticSiteEditor|A merge request was created:') }}
           <gl-link :href="links.mergeRequest.url">{{ links.mergeRequest.label }}</gl-link>
         </li>
       </ul>
