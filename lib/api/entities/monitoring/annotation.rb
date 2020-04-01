32798b1ccdd4c9b6d2b7b1a7bc84d8ae3c59c92b
Kirstie Cook <kcook@gitlab.com>
2020-04-01 11:52:57 -0500

    Fix annotation entity

--- lib/api/entities/monitoring/annotation.rb
+++ lib/api/entities/monitoring/annotation.rb
@@ -2,15 +2,17 @@
 
 module API
   module Entities
-    class Annotation < Grape::Entity
-      expose :id
-      expose :from
-      expose :to
-      expose :dashboard_id
-      expose :panel_id
-      expose :description
-      expose :environment_id
-      expose :cluster_id
+    module Monitoring
+      class Annotation < Grape::Entity
+        expose :id
+        expose :from
+        expose :to
+        expose :dashboard_id
+        expose :panel_id
+        expose :description
+        expose :environment_id
+        expose :cluster_id
+      end
     end
   end
 end
