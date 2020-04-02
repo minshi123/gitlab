140bcc3373b3b1b99a8047984f07dd88c3751839
Emily Ring <ering@gitlab.com>
2020-04-02 08:20:31 -0400

    Added endpoint prop

--- app/assets/javascripts/terraform/components/mr_widget_terraform_plan.vue
+++ app/assets/javascripts/terraform/components/mr_widget_terraform_plan.vue
@@ -43,10 +43,17 @@ export default {
       return __('Terraform plan output the following resources:');
     },
   },
+  props: {
+    endpoint: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 <template>
   <section class="mr-widget-section">
+    <h1>endpoint!! {{ endpoint }}</h1>
     <div class="mr-widget-body media d-flex flex-row">
       <span class="append-right-default align-self-start align-self-lg-center">
         <ci-icon :status="iconStatusObj" :size="24" />
