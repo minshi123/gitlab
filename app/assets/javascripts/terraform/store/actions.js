c2b4b30b1cf755500c166de725c81ba30f56c702
Emily Ring <ering@gitlab.com>
2020-04-02 09:25:22 -0400

    Added fecthPlans

--- app/assets/javascripts/terraform/store/actions.js
+++ app/assets/javascripts/terraform/store/actions.js
@@ -1,9 +1,34 @@
+import Poll from '~/lib/utils/poll';
 import Visibility from 'visibilityjs';
+import axios from '~/lib/utils/axios_utils';
+import flash from '~/flash';
 import * as types from './mutation_types';
 
 export const setEndpoint = ({ commit }, endpoint) => commit(types.SET_ENDPOINT, endpoint);
 
 export const fetchPlans = ({ state, commit }) => {
-  console.log('fetch time!! With my  new endpoint')
-  console.log(state.endpoint)
+  const poll = new Poll({
+    resource: {
+      fetchPlans: endpoint => axios.get(endpoint),
+    },
+    data: state.endpoint,
+    method: 'fetchPlans',
+    successCallback: ({ data }) => {
+      console.log('successCallback')
+      console.log(data)
+    },
+    errorCallback: () => flash(__('An error occurred while loading clusters')),
+  })
+
+  if (!Visibility.hidden()) {
+    poll.makeRequest();
+  }
+
+  Visibility.change(() => {
+    if (!Visibility.hidden()) {
+      poll.restart();
+    } else {
+      poll.stop();
+    }
+  });
 };
