73996b1afd8e2c3cdd13b0e79282db12cd25c48c
alinamihaila <amihaila@gitlab.com>
2020-04-07 10:30:18 +0300

    Move clusters app ingress to batch counting

--- ee/lib/ee/gitlab/usage_data.rb
+++ ee/lib/ee/gitlab/usage_data.rb
@@ -204,7 +204,7 @@ module EE
           {
             clusters_applications_cert_managers: ::Clusters::Applications::CertManager.where(time_period).distinct_by_user,
             clusters_applications_helm: ::Clusters::Applications::Helm.where(time_period).distinct_by_user,
-            clusters_applications_ingress: ::Clusters::Applications::Ingress.where(time_period).distinct_by_user,
+            clusters_applications_ingress: distinct_count(::Clusters::Applications::Ingress.where(time_period).available.joins(:cluster), 'clusters.user_id'),
             clusters_applications_knative: ::Clusters::Applications::Knative.where(time_period).distinct_by_user,
             clusters_disabled: distinct_count(::Clusters::Cluster.disabled.where(time_period), :user_id),
             clusters_enabled: distinct_count(::Clusters::Cluster.enabled.where(time_period), :user_id),
