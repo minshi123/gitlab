b266b678a183fecdc3c72c627edd31c13ecff71f
Alexander Turinske <aturinske@gitlab.com>
2020-04-10 14:39:16 -0600

    Add edit button to instance dashboard
    - bring in necessary Vuex store for project selector
    - add button to toggle project selector and rest of page
    - add tests

--- ee/app/assets/javascripts/security_dashboard/components/first_class_instance_security_dashboard.vue
+++ ee/app/assets/javascripts/security_dashboard/components/first_class_instance_security_dashboard.vue
@@ -1,9 +1,15 @@
 <script>
+import { s__ } from '~/locale';
+import { mapActions } from 'vuex';
+import { GlDeprecatedButton } from '@gitlab/ui';
 import SecurityDashboardLayout from 'ee/security_dashboard/components/security_dashboard_layout.vue';
 import InstanceSecurityVulnerabilities from './first_class_instance_security_dashboard_vulnerabilities.vue';
+import ProjectManager from './project_manager.vue';
 
 export default {
   components: {
+    GlDeprecatedButton,
+    ProjectManager,
     SecurityDashboardLayout,
     InstanceSecurityVulnerabilities,
   },
@@ -16,13 +22,64 @@ export default {
       type: String,
       required: true,
     },
+    projectAddEndpoint: {
+      type: String,
+      required: true,
+    },
+    projectListEndpoint: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      showProjectSelector: false,
+    };
+  },
+  computed: {
+    toggleButtonProps() {
+      return this.showProjectSelector
+        ? {
+            variant: 'success',
+            text: s__('SecurityDashboard|Return to dashboard'),
+          }
+        : {
+            variant: 'secondary',
+            text: s__('SecurityDashboard|Edit dashboard'),
+          };
+    },
+  },
+  created() {
+    this.setProjectEndpoints({
+      add: this.projectAddEndpoint,
+      list: this.projectListEndpoint,
+    });
+  },
+  methods: {
+    ...mapActions('projectSelector', ['setProjectEndpoints', 'fetchProjects']),
+    toggleProjectSelector() {
+      this.showProjectSelector = !this.showProjectSelector;
+    },
   },
 };
 </script>
 
 <template>
   <security-dashboard-layout>
+    <template #header>
+      <header class="page-title-holder flex-fill d-flex align-items-center">
+        <h2 class="page-title">{{ s__('SecurityDashboard|Security Dashboard') }}</h2>
+        <gl-deprecated-button
+          class="page-title-controls js-project-selector-toggle"
+          :variant="toggleButtonProps.variant"
+          @click="toggleProjectSelector"
+          v-text="toggleButtonProps.text"
+        />
+      </header>
+    </template>
+    <project-manager v-if="showProjectSelector" />
     <instance-security-vulnerabilities
+      v-else
       :dashboard-documentation="dashboardDocumentation"
       :empty-state-svg-path="emptyStateSvgPath"
     />
--- ee/spec/frontend/security_dashboard/components/first_class_instance_security_dashboard_spec.js
+++ ee/spec/frontend/security_dashboard/components/first_class_instance_security_dashboard_spec.js
@@ -1,36 +1,119 @@
-import { shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import FirstClassInstanceDashboard from 'ee/security_dashboard/components/first_class_instance_security_dashboard.vue';
 import FirstClassInstanceVulnerabilities from 'ee/security_dashboard/components/first_class_instance_security_dashboard_vulnerabilities.vue';
+import ProjectManager from 'ee/security_dashboard/components/project_manager.vue';
+import SecurityDashboardLayout from 'ee/security_dashboard/components/security_dashboard_layout.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('First Class Group Dashboard Component', () => {
+  let store;
   let wrapper;
 
   const dashboardDocumentation = 'dashboard-documentation';
   const emptyStateSvgPath = 'empty-state-path';
+  const projectAddEndpoint = 'projectAddEndpoint';
+  const projectListEndpoint = 'projectListEndpoint';
 
-  const findGroupVulnerabilities = () => wrapper.find(FirstClassInstanceVulnerabilities);
+  const findInstanceVulnerabilities = () => wrapper.find(FirstClassInstanceVulnerabilities);
 
-  const createWrapper = () => {
+  const defaultData = {
+    showProjectSelector: false,
+  };
+
+  const createWrapper = ({ data = defaultData }) => {
+    store = new Vuex.Store({
+      modules: {
+        projectSelector: {
+          namespaced: true,
+          actions: {
+            fetchProjects() {},
+            setProjectEndpoints() {},
+          },
+          state: {
+            projects: [],
+          },
+        },
+      },
+    });
+
     return shallowMount(FirstClassInstanceDashboard, {
+      localVue,
+      store,
       propsData: {
         dashboardDocumentation,
         emptyStateSvgPath,
+        projectAddEndpoint,
+        projectListEndpoint,
       },
+      data: () => data,
+      stubs: { SecurityDashboardLayout },
     });
   };
 
+  const findProjectSelectorToggleButton = () => wrapper.find('.js-project-selector-toggle');
+
   beforeEach(() => {
-    wrapper = createWrapper();
+    wrapper = createWrapper({});
   });
 
   afterEach(() => {
     wrapper.destroy();
   });
 
-  it('should render correctly', () => {
-    expect(findGroupVulnerabilities().props()).toEqual({
+  it('should render correctly on load', () => {
+    expect(wrapper.find('.page-title').text()).toBe('Security Dashboard');
+    expect(findInstanceVulnerabilities().props()).toEqual({
       dashboardDocumentation,
       emptyStateSvgPath,
+    });
+    expect(findProjectSelectorToggleButton().exists()).toBe(true);
+    expect(wrapper.contains(ProjectManager)).toBe(false);
+  });
+
+  it('should render correctly during project selection', () => {
+    wrapper = createWrapper({ data: { showProjectSelector: true } });
+    expect(findProjectSelectorToggleButton().exists()).toBe(true);
+    expect(findInstanceVulnerabilities().exists()).toBe(false);
+    expect(wrapper.contains(ProjectManager)).toBe(true);
+  });
+
+  describe('methods', () => {
+    describe('toggleProjectSelector', () => {
+      beforeEach(() => {
+        wrapper = createWrapper({});
+      });
+
+      it('should toggle the showProjectSelector from false to true', () => {
+        wrapper.vm.toggleProjectSelector();
+        expect(wrapper.vm.showProjectSelector).toBe(true);
+
+      })
+
+      it('should toggle the showProjectSelector from true to false', () => {
+        wrapper = createWrapper({ data: { showProjectSelector: true } });
+        wrapper.vm.toggleProjectSelector();
+        expect(wrapper.vm.showProjectSelector).toBe(false);
+      })
+    })
+  });
+
+  describe('computed', () => {
+    describe('toggleButtonProps', () => {
+      beforeEach(() => {
+        wrapper = createWrapper({});
+      });
+
+      it('returns the appropriate message when showProjectSelector is false', () => {
+        expect(wrapper.vm.toggleButtonProps.text).toBe('Edit dashboard');
+      });
+
+      it('returns the appropriate message when showProjectSelector is true', () => {
+        wrapper = createWrapper({ data: { showProjectSelector: true } });
+        expect(wrapper.vm.toggleButtonProps.text).toBe('Return to dashboard');
+      });
     });
   });
 });
