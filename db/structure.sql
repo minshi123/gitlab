a439a7ab11023af7e030e8fc06f24da3e0b65c2b
alinamihaila <amihaila@gitlab.com>
2020-04-14 19:21:50 +0300

    Remove redundant index in projects table

--- db/structure.sql
+++ db/structure.sql
@@ -9914,8 +9914,6 @@ CREATE INDEX index_projects_api_vis20_updated_at ON public.projects USING btree 
 
 CREATE INDEX index_projects_on_created_at_and_id ON public.projects USING btree (created_at, id);
 
-CREATE INDEX index_projects_on_creator_id_and_created_at ON public.projects USING btree (creator_id, created_at);
-
 CREATE INDEX index_projects_on_creator_id_and_created_at_and_id ON public.projects USING btree (creator_id, created_at, id);
 
 CREATE INDEX index_projects_on_description_trigram ON public.projects USING gin (description public.gin_trgm_ops);
@@ -13132,5 +13130,6 @@ COPY "schema_migrations" (version) FROM STDIN;
 20200407094923
 20200408110856
 20200408175424
+20200414144547
 \.
 
