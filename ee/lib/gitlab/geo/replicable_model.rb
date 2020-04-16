2cec90e989170f7d2b8ce6820e379617f7c70a00
Gabriel Mazetto <gabriel@gitlab.com>
2020-04-16 04:11:00 +0200

    Improve Geo Framework code documentation

--- ee/lib/gitlab/geo/replicable_model.rb
+++ ee/lib/gitlab/geo/replicable_model.rb
@@ -12,6 +12,9 @@ module Gitlab
       end
 
       class_methods do
+        # Associate current model with specified replicator
+        #
+        # @param [Gitlab::Geo::Replicator] klass
         def with_replicator(klass)
           raise ArgumentError, 'Must be a class inheriting from Gitlab::Geo::Replicator' unless klass < ::Gitlab::Geo::Replicator
 
@@ -25,11 +28,13 @@ module Gitlab
 
       # Geo Replicator
       #
+      # @abstract
       # @return [Gitlab::Geo::Replicator]
       def replicator
         raise NotImplementedError, 'There is no Replicator defined for this model'
       end
 
+      # Clear model verification checksum and force recalculation
       def calculate_checksum!
         self.verification_checksum = nil
 
@@ -38,10 +43,20 @@ module Gitlab
         self.verification_checksum = self.class.hexdigest(file.path)
       end
 
+      # Checks whether model needs checksum to be performed
+      #
+      # Conditions:
+      # - No checksum is present
+      # - It's capable of generating a checksum of itself
+      #
+      # @return [Boolean]
       def needs_checksum?
         verification_checksum.nil? && checksummable?
       end
 
+      # Return whether its capable of generating a checksum of itself
+      #
+      # @return [Boolean] whether it can generate a checksum
       def checksummable?
         local? && file_exist?
       end
