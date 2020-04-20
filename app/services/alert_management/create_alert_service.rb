d0d0dd8597da803a5bdec3d418005127e3b6fcfb
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-04-20 17:03:14 +0200

    Store alert in database

--- app/services/alert_management/create_alert_service.rb
+++ app/services/alert_management/create_alert_service.rb
@@ -2,8 +2,47 @@
 
 module AlertManagement
   class CreateAlertService < BaseService
+    def initialize(project, params)
+      @payload_parser = params.fetch(:payload_parser, :invalid_payload_parser)
+      @raw_payload = params.except(:payload_parser)
+      super(project, nil, params)
+    end
+
     def execute
-      success
+      alert = AlertManagement::Alert.create!(alert_params)
+
+      ServiceResponse.success(payload: alert)
+    rescue payload_parser::BadPayloadError
+      bad_request
+    end
+
+    private
+
+    attr_reader :payload_parser, :raw_payload
+
+    def parsed_payload
+      @parsed_payload ||= payload_parser.call(raw_payload).with_indifferent_access
+    end
+
+    def alert_params
+      {
+        project: project,
+        started_at: parsed_payload['startsAt'],
+        title: annotations[:title],
+        description: annotations[:description],
+        monitoring_tool: annotations[:monitoring_tool],
+        service: annotations[:service],
+        hosts: annotations[:hosts],
+        payload: raw_payload
+      }
+    end
+
+    def annotations
+      @annotations ||= parsed_payload[:annotations]
+    end
+
+    def bad_request
+      ServiceResponse.error(message: 'Bad Request', http_status: :bad_request)
     end
   end
 end
--- spec/services/alert_management/create_alert_service_spec.rb
+++ spec/services/alert_management/create_alert_service_spec.rb
@@ -4,18 +4,69 @@ require 'spec_helper'
 
 describe AlertManagement::CreateAlertService do
   let_it_be(:project) { create(:project, :repository, :private) }
-  let(:service) { described_class.new(project, alert_payload) }
-  let(:alert_payload) do
+  let(:service) { described_class.new(project, create_alert_params) }
+
+  let(:payload) do
     {
-      title: 'Alert title'
+      'title' => 'Alert title',
+      'description' => 'Description',
+      'monitoring_tool' => 'Monitoring tool name',
+      'service' => 'Service',
+      'hosts' => ['gitlab.com'],
+      'some' => { 'extra' => { 'payload' => 'here' } }
     }
   end
 
-  subject { service.execute }
+  let(:create_alert_params) { payload.merge(payload_parser: Gitlab::Alerting::NotificationPayloadParser) }
+
+  subject(:create_alert) { service.execute }
 
   describe '#execute' do
-    it 'returns success' do
-      is_expected.to eq(status: :success)
+    context 'with valid payload' do
+      it 'returns success' do
+        is_expected.to be_success
+      end
+
+      it 'creates an alert' do
+        expect { create_alert }.to change(AlertManagement::Alert, :count).by(1)
+      end
+
+      it 'created alert has all data properly assigned' do
+        create_alert
+
+        alert = AlertManagement::Alert.last
+        alert_attributes = alert.attributes.except('id', 'iid', 'created_at', 'updated_at')
+
+        expect(alert_attributes).to eq(
+          'project_id' => project.id,
+          'issue_id' => nil,
+          'fingerprint' => nil,
+          'title' => 'Alert title',
+          'description' => 'Description',
+          'monitoring_tool' => 'Monitoring tool name',
+          'service' => 'Service',
+          'hosts' => ['gitlab.com'],
+          'payload' => payload,
+          'severity' => 'critical',
+          'status' => 'triggered',
+          'events' => 1,
+          'started_at' => alert.started_at,
+          'ended_at' => nil
+        )
+      end
+    end
+
+    context 'with invalid payload' do
+      before do
+        allow(Gitlab::Alerting::NotificationPayloadParser)
+          .to receive(:call)
+          .and_raise(Gitlab::Alerting::NotificationPayloadParser::BadPayloadError)
+      end
+
+      it 'returns error' do
+        expect(create_alert).to be_error
+        expect(create_alert.http_status).to eq(:bad_request)
+      end
     end
   end
 end
