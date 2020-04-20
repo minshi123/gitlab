110f3712ffb4b421c1424785ffdf729122ba7ab4
Denys Mishunov <dmishunov@gitlab.com>
2020-04-20 23:24:08 +0200

    Moved out the snippet-related selector
    TO keep the title component shared without traces of snippet-related
    info, moved the selector out to the top level edit form.

--- app/assets/javascripts/vue_shared/components/form/title.vue
+++ app/assets/javascripts/vue_shared/components/form/title.vue
@@ -6,10 +6,11 @@ export default {
     GlFormInput,
     GlFormGroup,
   },
+  inheritAttrs: false,
 };
 </script>
 <template>
   <gl-form-group :label="__('Title')" label-for="title-field-edit">
-    <gl-form-input data-qa-selector="snippet_title" v-bind="$attrs" v-on="$listeners" />
+    <gl-form-input v-bind="$attrs" v-on="$listeners" />
   </gl-form-group>
 </template>
