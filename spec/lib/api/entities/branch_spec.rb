8d21e577e7d5ab7f9f0709b71048582d44595f31
Vasilii Iakliushin <viakliushin@gitlab.com>
2020-04-22 11:17:58 +0200

    Add test for API::Entities::Branch

---
+++ spec/lib/api/entities/branch_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe API::Entities::Branch do
+  describe '#as_json' do
+    subject { entity.as_json }
+
+    let(:project) { create(:project, :public, :repository) }
+    let(:repository) { project.repository }
+    let(:branch) { repository.find_branch('master') }
+    let(:entity) { described_class.new(branch, project: project) }
+
+    it 'includes basic fields', :aggregate_failures do
+      is_expected.to include(
+        name: 'master',
+        commit: a_kind_of(Hash),
+        merged: false,
+        protected: false,
+        developers_can_push: false,
+        developers_can_merge: false,
+        can_push: false,
+        default: true
+      )
+    end
+  end
+end
