c0ca0dc187c0343583ce6596504d730127a54a2b
Mario de la Ossa <mariodelaossa@gmail.com>
2020-04-21 20:36:00 -0600

    Add Sprint relationships and constraints
    Adds Sprints to projects, groups, issues, and merge requests.
    Also adds foreign keys, some special constraints, etc.

--- db/structure.sql
+++ db/structure.sql
@@ -3303,7 +3303,8 @@ CREATE TABLE public.issues (
     duplicated_to_id integer,
     promoted_to_epic_id integer,
     health_status smallint,
-    external_key character varying(255)
+    external_key character varying(255),
+    sprint_id bigint
 );
 
 CREATE SEQUENCE public.issues_id_seq
@@ -3878,7 +3879,8 @@ CREATE TABLE public.merge_requests (
     allow_maintainer_to_push boolean,
     state_id smallint DEFAULT 1 NOT NULL,
     rebase_jid character varying,
-    squash_commit_sha bytea
+    squash_commit_sha bytea,
+    sprint_id bigint
 );
 
 CREATE TABLE public.merge_requests_closing_issues (
@@ -6036,6 +6038,7 @@ CREATE TABLE public.sprints (
     title_html text,
     description text,
     description_html text,
+    CONSTRAINT sprints_must_belong_to_project_or_group CHECK ((((project_id <> NULL::bigint) AND (group_id IS NULL)) OR ((group_id <> NULL::bigint) AND (project_id IS NULL)))),
     CONSTRAINT sprints_state CHECK ((char_length(state) <= 255)),
     CONSTRAINT sprints_title CHECK ((char_length(title) <= 255))
 );
@@ -9511,6 +9514,8 @@ CREATE INDEX index_issues_on_promoted_to_epic_id ON public.issues USING btree (p
 
 CREATE INDEX index_issues_on_relative_position ON public.issues USING btree (relative_position);
 
+CREATE INDEX index_issues_on_sprint_id ON public.issues USING btree (sprint_id);
+
 CREATE INDEX index_issues_on_title_trigram ON public.issues USING gin (title public.gin_trgm_ops);
 
 CREATE INDEX index_issues_on_updated_at ON public.issues USING btree (updated_at);
@@ -9672,6 +9677,8 @@ CREATE INDEX index_merge_requests_on_milestone_id ON public.merge_requests USING
 CREATE INDEX index_merge_requests_on_source_branch ON public.merge_requests USING btree (source_branch);
 
 CREATE INDEX index_merge_requests_on_source_project_id_and_source_branch ON public.merge_requests USING btree (source_project_id, source_branch);
+
+CREATE INDEX index_merge_requests_on_sprint_id ON public.merge_requests USING btree (sprint_id);
 
 CREATE INDEX index_merge_requests_on_target_branch ON public.merge_requests USING btree (target_branch);
 
@@ -10743,6 +10750,9 @@ ALTER TABLE ONLY public.push_event_payloads
 ALTER TABLE ONLY public.ci_builds
     ADD CONSTRAINT fk_3a9eaa254d FOREIGN KEY (stage_id) REFERENCES public.ci_stages(id) ON DELETE CASCADE;
 
+ALTER TABLE ONLY public.issues
+    ADD CONSTRAINT fk_3b8c72ea56 FOREIGN KEY (sprint_id) REFERENCES public.sprints(id) ON DELETE CASCADE;
+
 ALTER TABLE ONLY public.epics
     ADD CONSTRAINT fk_3c1fd1cccc FOREIGN KEY (due_date_sourcing_milestone_id) REFERENCES public.milestones(id) ON DELETE SET NULL;
 
@@ -10860,8 +10870,14 @@ ALTER TABLE ONLY public.vulnerabilities
 ALTER TABLE ONLY public.labels
     ADD CONSTRAINT fk_7de4989a69 FOREIGN KEY (project_id) REFERENCES public.projects(id) ON DELETE CASCADE;
 
+ALTER TABLE ONLY public.merge_requests
+    ADD CONSTRAINT fk_7e85395a64 FOREIGN KEY (sprint_id) REFERENCES public.sprints(id) ON DELETE CASCADE;
+
 ALTER TABLE ONLY public.merge_request_metrics
     ADD CONSTRAINT fk_7f28d925f3 FOREIGN KEY (merged_by_id) REFERENCES public.users(id) ON DELETE SET NULL;
+
+ALTER TABLE ONLY public.sprints
+    ADD CONSTRAINT fk_80aa8a1f95 FOREIGN KEY (group_id) REFERENCES public.namespaces(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY public.import_export_uploads
     ADD CONSTRAINT fk_83319d9721 FOREIGN KEY (group_id) REFERENCES public.namespaces(id) ON DELETE CASCADE;
@@ -11096,6 +11112,9 @@ ALTER TABLE ONLY public.namespaces
 
 ALTER TABLE ONLY public.fork_networks
     ADD CONSTRAINT fk_e7b436b2b5 FOREIGN KEY (root_project_id) REFERENCES public.projects(id) ON DELETE SET NULL;
+
+ALTER TABLE ONLY public.sprints
+    ADD CONSTRAINT fk_e8206c9686 FOREIGN KEY (project_id) REFERENCES public.projects(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY public.application_settings
     ADD CONSTRAINT fk_e8a145f3a7 FOREIGN KEY (instance_administrators_group_id) REFERENCES public.namespaces(id) ON DELETE SET NULL;
@@ -13155,6 +13174,10 @@ COPY "schema_migrations" (version) FROM STDIN;
 20200303055348
 20200303074328
 20200303181648
+20200304023245
+20200304023851
+20200304024025
+20200304024042
 20200304085423
 20200304090155
 20200304121828
@@ -13310,5 +13333,8 @@ COPY "schema_migrations" (version) FROM STDIN;
 20200416120128
 20200416120354
 20200420172113
+20200420172752
+20200420172927
+20200420201933
 \.
 
---
+++ spec/factories/sprints.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :sprint do
+    title
+
+    transient do
+      project { nil }
+      group { nil }
+      project_id { nil }
+      group_id { nil }
+      resource_parent { nil }
+      state { "active" }
+    end
+
+    trait :active do
+      state { "active" }
+    end
+
+    trait :closed do
+      state { "closed" }
+    end
+
+    trait :with_dates do
+      start_date { Date.new(2000, 1, 1) }
+      due_date { Date.new(2000, 1, 30) }
+    end
+
+    after(:build, :stub) do |sprint, evaluator|
+      if evaluator.group
+        sprint.group = evaluator.group
+      elsif evaluator.group_id
+        sprint.group_id = evaluator.group_id
+      elsif evaluator.project
+        sprint.project = evaluator.project
+      elsif evaluator.project_id
+        sprint.project_id = evaluator.project_id
+      elsif evaluator.resource_parent
+        id = evaluator.resource_parent.id
+        evaluator.resource_parent.is_a?(Group) ? evaluator.group_id = id : evaluator.project_id = id
+      else
+        sprint.project = create(:project)
+      end
+    end
+
+    factory :active_sprint, traits: [:active]
+    factory :closed_sprint, traits: [:closed]
+  end
+end
