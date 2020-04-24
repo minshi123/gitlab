5a3ca8c18cc15d48ea728811fe69c97c66d1cf79
Enrique Alcantara <ealcantara@gitlab.com>
2020-04-24 12:10:14 -0400

    Create VueRouter configuration
    Configure VueRouter by  creating the main page
    route and setting  the base URL

---
+++ app/assets/javascripts/static_site_editor/router/routes.js
@@ -0,0 +1,10 @@
+import Home from '../pages/index.vue';
+import { ROOT_ROUTE_NAME } from './constants';
+
+export default [
+  {
+    name: ROOT_ROUTE_NAME,
+    path: '/',
+    component: Home,
+  },
+];
