8e0b368b009996522a07bc52211845411e9d930b
Samantha Ming <sming@gitlab.com>
2020-04-24 19:33:25 -0700

    Add feature spec for user view commits
    Add test to ensure correct commits per day are displayed in the commits list

---
+++ spec/features/commits/user_view_commits_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Commit > User view commits', :js do
+  include RepoHelpers
+
+  let(:project) { create(:project, :public, :repository) }
+  let(:user) { project.creator }
+  let(:commit) { project.commit }
+
+  before do
+    project.add_maintainer(user)
+    sign_in(user)
+
+    visit project_commits_path(project)
+  end
+
+  describe 'Commits List' do
+    it 'displays the correct number of commits per day in the header' do
+      expect(first('.js-commit-header').find('.commits-count').text).to include('1')
+    end
+
+    it 'lists the correct number of commits' do
+      expect(page).to have_selector('#commits-list > li:nth-child(2) > ul', count: 1)
+    end
+  end
+end
