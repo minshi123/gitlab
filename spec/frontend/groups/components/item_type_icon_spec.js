e47c2707a6caf709cbc8cefd7e6de4002a685875
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-04-28 12:15:13 +0200

    Move item_type_icon to jest

--- spec/frontend/groups/components/item_type_icon_spec.js
+++ spec/frontend/groups/components/item_type_icon_spec.js
@@ -1,23 +1,24 @@
 import Vue from 'vue';
 
-import mountComponent from 'spec/helpers/vue_mount_component_helper';
+import mountComponent from 'helpers/vue_mount_component_helper';
 import itemTypeIconComponent from '~/groups/components/item_type_icon.vue';
 import { ITEM_TYPE } from '../mock_data';
 
 const createComponent = (itemType = ITEM_TYPE.GROUP, isGroupOpen = false) => {
   const Component = Vue.extend(itemTypeIconComponent);
 
-  return mountComponent(Component, {
+  const vm = mountComponent(Component, {
     itemType,
     isGroupOpen,
   });
+  vm.$mount();
+  return vm;
 };
 
 describe('ItemTypeIconComponent', () => {
   describe('template', () => {
     it('should render component template correctly', () => {
       const vm = createComponent();
-      vm.$mount();
 
       expect(vm.$el.classList.contains('item-type-icon')).toBeTruthy();
       vm.$destroy();
@@ -27,13 +28,11 @@ describe('ItemTypeIconComponent', () => {
       let vm;
 
       vm = createComponent(ITEM_TYPE.GROUP, true);
-      vm.$mount();
 
       expect(vm.$el.querySelector('use').getAttribute('xlink:href')).toContain('folder-open');
       vm.$destroy();
 
       vm = createComponent(ITEM_TYPE.GROUP);
-      vm.$mount();
 
       expect(vm.$el.querySelector('use').getAttribute('xlink:href')).toContain('folder');
       vm.$destroy();
@@ -43,13 +42,11 @@ describe('ItemTypeIconComponent', () => {
       let vm;
 
       vm = createComponent(ITEM_TYPE.PROJECT);
-      vm.$mount();
 
       expect(vm.$el.querySelector('use').getAttribute('xlink:href')).toContain('bookmark');
       vm.$destroy();
 
       vm = createComponent(ITEM_TYPE.GROUP);
-      vm.$mount();
 
       expect(vm.$el.querySelector('use').getAttribute('xlink:href')).not.toContain('bookmark');
       vm.$destroy();
