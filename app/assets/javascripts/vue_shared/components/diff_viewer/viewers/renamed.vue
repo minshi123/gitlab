443a2834a4c19cff745ad46049f594c4990c365c
Thomas Randolph <trandolph@gitlab.com>
2020-04-29 17:13:50 -0600

    Show a link in the UI to load the content of a `renamed` file

--- app/assets/javascripts/vue_shared/components/diff_viewer/viewers/renamed.vue
+++ app/assets/javascripts/vue_shared/components/diff_viewer/viewers/renamed.vue
@@ -1,3 +1,30 @@
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: {
+      diffFile: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      ...mapActions('diffs', [
+        'switchToFullDiffFromRenamedFile',
+      ]),
+    },
+  };
+</script>
+
 <template>
-  <div class="nothing-here-block">{{ __('File moved') }}</div>
+  <div class="nothing-here-block">
+    {{ __('File moved') }}
+    <a
+      v-if="diffFile.alternate_viewer.name === 'text'"
+      href="#"
+      @click.prevent="switchToFullDiffFromRenamedFile( { diffFile } )"
+    >
+      {{ __('(Show file)') }}
+    </a>
+  </div>
 </template>
