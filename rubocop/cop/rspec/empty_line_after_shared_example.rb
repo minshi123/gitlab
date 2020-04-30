5a5b3de9211c179766bae9c8e7cd38848a6f378b
Doug Stull <dstull@gitlab.com>
2020-04-30 12:55:35 -0400

    Simplify code and update documentation
    - resolve notify spec

--- rubocop/cop/rspec/empty_line_after_shared_example.rb
+++ rubocop/cop/rspec/empty_line_after_shared_example.rb
@@ -12,44 +12,37 @@ module RuboCop
       # @example
       #   # bad
       #   RSpec.describe Foo do
-      #     it 'does this' do
+      #     it_behaves_like 'do this first'
+      #     it_behaves_like 'does this' do
       #     end
-      #     it 'does that' do
+      #     it_behaves_like 'does that' do
       #     end
+      #     it_behaves_like 'do some more'
       #   end
       #
       #   # good
       #   RSpec.describe Foo do
-      #     it 'does this' do
+      #     it_behaves_like 'do this first'
+      #     it_behaves_like 'does this' do
       #     end
       #
-      #     it 'does that' do
+      #     it_behaves_like 'does that' do
       #     end
+      #
+      #     it_behaves_like 'do some more'
       #   end
       #
-      #   # fair - it's ok to have non-separated one-liners
+      #   # fair - it's ok to have non-separated without blocks
       #   RSpec.describe Foo do
-      #     it { one }
-      #     it { two }
-      #   end
-      #
-      # @example with AllowConsecutiveOneLiners configuration
-      #
-      #   # rubocop.yml
-      #   # RSpec/EmptyLineAfterExample:
-      #   #   AllowConsecutiveOneLiners: false
-      #
-      #   # bad
-      #   RSpec.describe Foo do
-      #     it { one }
-      #     it { two }
+      #     it_behaves_like 'do this first'
+      #     it_behaves_like 'does this'
       #   end
       #
       class EmptyLineAfterSharedExample < RuboCop::Cop::Cop
         include RuboCop::RSpec::BlankLineSeparation
         include RuboCop::RSpec::Language
 
-        MSG = 'Add an empty line after `%<example>s`.'
+        MSG = 'Add an empty line after `%<example>s` block.'
 
         def_node_matcher :shared_examples,
                          (SharedGroups::ALL + Includes::ALL).block_pattern
@@ -57,7 +50,6 @@ module RuboCop
         def on_block(node)
           shared_examples(node) do
             return if last_child?(node)
-            return if allowed_one_liner?(node)
 
             missing_separating_line(node) do |location|
               add_offense(node,
@@ -65,30 +57,6 @@ module RuboCop
                           message: format(MSG, example: node.method_name))
             end
           end
-        end
-
-        def allowed_one_liner?(node)
-          consecutive_one_liner?(node) && allow_consecutive_one_liners?
-        end
-
-        def allow_consecutive_one_liners?
-          cop_config['AllowConsecutiveOneLiners']
-        end
-
-        def consecutive_one_liner?(node)
-          node.line_count == 1 && next_one_line_example?(node)
-        end
-
-        def next_one_line_example?(node)
-          next_sibling = next_sibling(node)
-          return unless next_sibling
-          return unless example?(next_sibling)
-
-          next_sibling.line_count == 1
-        end
-
-        def next_sibling(node)
-          node.parent.children[node.sibling_index + 1]
         end
       end
     end
