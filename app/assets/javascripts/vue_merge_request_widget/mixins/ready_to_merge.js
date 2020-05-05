1681fb7074f4dc1b1bd68dd34ef283bd3c5014b1
Frederic Caplette <fcaplette@gitlab.com>
2020-05-05 13:02:30 -0400

    Handle conflict no CI and pipelines must succeed
    When there are no CI services found and
    the pipeline must succeed option is turned
    on, this is considered an invalid usecase
    since the pipeline will never be successful.
    Because of this, we add a meaningful message
    to the UI that explain this situation and link
    to our documentation while in the Merge request.

--- app/assets/javascripts/vue_merge_request_widget/mixins/ready_to_merge.js
+++ app/assets/javascripts/vue_merge_request_widget/mixins/ready_to_merge.js
@@ -1,20 +1,26 @@
 import { __ } from '~/locale';
 
 export const MERGE_DISABLED_TEXT = __('You can only merge once the items above are resolved.');
+export const PIPELINE_MUST_SUCCEED_CONFLICT_TEXT = __(
+  'Pipelines must succeed for merge requests to be eligible to merge. Please enable pipelines for this project to continue. For more information, see the %{linkStart}documentation.%{linkEnd}',
+);
 
 export default {
   computed: {
     isMergeButtonDisabled() {
-      const { commitMessage } = this;
+      const { commitMessage, mr } = this;
       return Boolean(
         !commitMessage.length ||
           !this.shouldShowMergeControls ||
           this.isMakingRequest ||
-          this.mr.preventMerge,
+          !mr.isApproved,
       );
     },
     mergeDisabledText() {
       return MERGE_DISABLED_TEXT;
+    },
+    pipelineMustSucceedConflictText() {
+      return PIPELINE_MUST_SUCCEED_CONFLICT_TEXT;
     },
     autoMergeText() {
       // MWPS is currently the only auto merge strategy available in CE
