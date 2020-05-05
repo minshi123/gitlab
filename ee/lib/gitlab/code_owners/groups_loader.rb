59cf05107d870374cb9680ee663aa4defbfbe6c0
Kerri Miller <kerrizor@kerrizor.com>
2020-05-05 13:53:55 -0700

    Add project group and ancestors to group list
    This addresses an issue that arises when the owner of a path is the
    group under which the project lives (such as `@gitlab-org` for the
    Gitlab project..) Gitlab::CodeOwners::GroupsLoader did not add that
    group to the list of groups when attempting to determine ownership. This
    list of groups is expanded into a list of users that the user
    attempting the action (typically a merge) is compared to. Sinc the
    project's group is not added to the group list, its members are not
    expanded and are thus missing from the list of owners for a given path.
    
    Additionally, this will look not simply at the immediate group of the
    project, but at the entire group tree ancestors to the root group. For
    example, if the "merge_service" project is at
    
    gitlab-org/engineering/source_code/merge_service
    
    ..then any of these groups would be considerable available for listing
    as a code owner without having to be invited to the project:
    
    @gitlab-org
    @gitlab-org/engineering
    @gitlab-org/engineering/source_code
    
    This address both the proximate cause of
    https://gitlab.com/gitlab-org/gitlab/-/issues/216345 but of the
    long-requested feature https://gitlab.com/gitlab-org/gitlab/-/issues/32432

--- ee/lib/gitlab/code_owners/groups_loader.rb
+++ ee/lib/gitlab/code_owners/groups_loader.rb
@@ -23,7 +23,25 @@ module Gitlab
         return Group.none if extractor.names.empty?
 
         groups = project.invited_groups.where_full_path_in(extractor.names)
-        groups.with_route.with_users
+
+        if Feature.enabled?(:codeowners_match_ancestor_groups, project, default_enabled: false)
+          group_list = groups.with_route.with_users.to_a
+
+          if project.group
+            ancestor_groups = project.group.self_and_ancestors.with_route.with_users
+
+            # If the project.group's ancestor group(s) are listed as owners, add
+            #   them to group_list
+            #
+            if (extractor.names & ancestor_groups.collect(&:full_path)).any?
+              group_list << ancestor_groups
+            end
+          end
+
+          group_list.flatten
+        else
+          groups.with_route.with_users
+        end
       end
     end
   end
