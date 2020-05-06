8b4d4c14ef2177ebda37e969e65647e502b2a75a
Patrick Derichs <pderichs@gitlab.com>
2020-05-06 16:43:47 +0200

    Add StateNote synthetic note

---
+++ app/models/state_note.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class StateNote < SyntheticNote
+  def self.from_event(event, resource: nil, resource_parent: nil)
+    attrs = note_attributes(event.state, event, resource, resource_parent)
+
+    StateNote.new(attrs)
+  end
+
+  def note_html
+    @note_html ||= "<p dir=\"auto\">#{note_text(html: true)}</p>"
+  end
+
+  private
+
+  def note_text(html: false)
+    if event.reopened?
+      'reopened'
+    elsif event.closed?
+      'closed'
+    else
+      event.state
+    end
+  end
+end
