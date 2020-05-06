423ea8295f6835507837eac81ac894afa70be745
Enrique Alcantara <ealcantara@gitlab.com>
2020-05-06 17:43:45 -0400

    Add file client-side resolver
    Create a client-side resolver to
    retrieve a file content using the GraphQL
    API

--- app/assets/javascripts/static_site_editor/graphql/index.js
+++ app/assets/javascripts/static_site_editor/graphql/index.js
@@ -3,11 +3,17 @@ import VueApollo from 'vue-apollo';
 import createDefaultClient from '~/lib/graphql';
 import typeDefs from './typedefs.graphql';
 
+import fileResolver from './resolvers/file';
+
 Vue.use(VueApollo);
 
 const createApolloProvider = data => {
   const defaultClient = createDefaultClient(
-    {},
+    {
+      Project: {
+        file: fileResolver,
+      },
+    },
     {
       typeDefs,
     },
