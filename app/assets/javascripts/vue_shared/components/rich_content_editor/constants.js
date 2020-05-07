31109b3a8d54e75dd94d5fd71889f6e81c32e2c0
jerasmus <jerasmus@gitlab.com>
2020-05-07 16:22:35 +0200

    Defined custom toolbar items
    Defined the custom toolbar items/icons

--- app/assets/javascripts/vue_shared/components/rich_content_editor/constants.js
+++ app/assets/javascripts/vue_shared/components/rich_content_editor/constants.js
@@ -1,24 +1,31 @@
+import { __ } from '~/locale';
+import { generateToolbarItem } from './toolbar_service';
+
+/* eslint-disable @gitlab/require-i18n-strings */
+const TOOLBAR_ITEM_CONFIGS = [
+  { icon: 'heading', event: 'openHeadingSelect', class: 'tui-heading', tooltip: __('Headings') },
+  { icon: 'bold', command: 'Bold', tooltip: __('Bold') },
+  { icon: 'italic', command: 'Italic', tooltip: __('Italic') },
+  { icon: 'strikethrough', command: 'Strike', tooltip: __('Strike') },
+  { isDivider: true },
+  { icon: 'quote', command: 'Blockquote', tooltip: __('Quote') },
+  { icon: 'link', event: 'openPopupAddLink', tooltip: __('Insert link') },
+  { icon: 'doc-code', command: 'CodeBlock', tooltip: __('Insert code block') },
+  { isDivider: true },
+  { icon: 'list-bulleted', command: 'UL', tooltip: __('Unordered list') },
+  { icon: 'list-numbered', command: 'OL', tooltip: __('Ordered list') },
+  { icon: 'list-task', command: 'Task', tooltip: __('Task') },
+  { icon: 'list-indent', command: 'Indent', tooltip: __('Indent') },
+  { icon: 'list-outdent', command: 'Outdent', tooltip: __('Outdent') },
+  { isDivider: true },
+  { icon: 'dash', command: 'HR', tooltip: __('Line') },
+  { icon: 'table', event: 'openPopupAddTable', class: 'tui-table', tooltip: __('Insert table') },
+  { isDivider: true },
+  { icon: 'code', command: 'Code', tooltip: __('Inline code') },
+];
+
 export const EDITOR_OPTIONS = {
-  toolbarItems: [
-    'heading',
-    'bold',
-    'italic',
-    'strike',
-    'divider',
-    'quote',
-    'link',
-    'codeblock',
-    'divider',
-    'ul',
-    'ol',
-    'task',
-    'indent',
-    'outdent',
-    'divider',
-    'hr',
-    'table',
-    'code',
-  ],
+  toolbarItems: TOOLBAR_ITEM_CONFIGS.map(config => generateToolbarItem(config)),
 };
 
 export const EDITOR_TYPES = {
--- app/assets/javascripts/vue_shared/components/rich_content_editor/toolbar_service.js
+++ app/assets/javascripts/vue_shared/components/rich_content_editor/toolbar_service.js
@@ -3,19 +3,20 @@ import ToolbarItem from './toolbar_item.vue';
 
 const createWrapper = (icon, className) => {
   const Component = Vue.extend(ToolbarItem);
-  const instance = new Component({
-    propsData: {
-      className,
-      icon,
-    },
-  });
+  const instance = new Component({ propsData: { className, icon } });
 
   instance.$mount();
   return instance.$el;
 };
 
 // eslint-disable-next-line import/prefer-default-export
-export const customToolbarItem = ({ icon, className, event, command, tooltip }) => {
+export const generateToolbarItem = config => {
+  const { icon, class: className, event, command, tooltip, isDivider } = config;
+
+  if (isDivider) {
+    return 'divider';
+  }
+
   return {
     type: 'button',
     options: {
