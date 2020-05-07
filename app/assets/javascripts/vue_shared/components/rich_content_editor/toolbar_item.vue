c98c763c1fcf8b290ba8f01308532b7e87c0b186
jerasmus <jerasmus@gitlab.com>
2020-05-07 15:11:31 +0200

    Add toolbarItem component
    Added a custom toolbar item component

---
+++ app/assets/javascripts/vue_shared/components/rich_content_editor/toolbar_item.vue
@@ -0,0 +1,25 @@
+<script>
+import { GlIcon } from '@gitlab/ui';
+
+export default {
+  components: {
+    GlIcon,
+  },
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    className: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+};
+</script>
+<template>
+  <button class="text-dark" :class="className">
+    <gl-icon :name="icon" />
+  </button>
+</template>
