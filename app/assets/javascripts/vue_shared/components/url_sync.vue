69b0f6f7eb3345564dae8cabb0b50e8b179227b9
samdbeckham <sbeckham@gitlab.com>
2020-05-07 16:31:01 +0100

    WIP: adds a url mixin component

---
+++ app/assets/javascripts/vue_shared/components/url_sync.vue
@@ -0,0 +1,21 @@
+<script>
+import { historyPushState } from '~/lib/utils/common_utils';
+import { setUrlParams } from '~/lib/utils/url_utility';
+
+export default {
+  props: {
+    query: {
+      type: String,
+      required: true
+    }
+  },
+  watch: {
+    query: {
+      deep: true,
+      handler() {
+        historyPushState(setUrlParams(this.query, window.location.href, true));
+      }
+    },
+  },
+};
+</script>
