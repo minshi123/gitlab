94c9ef202ff5535550a23d781c9385fc4a2f5ad0
Daniel Tian <dtian@gitlab.com>
2020-05-08 02:33:13 -1000

    Update header and table for security config page
    Update the header and use GlTable for the the security configuration
    page

--- ee/spec/frontend/security_configuration/components/app_spec.js
+++ ee/spec/frontend/security_configuration/components/app_spec.js
@@ -1,12 +1,18 @@
 import { shallowMount } from '@vue/test-utils';
-
-import { GlLink } from '@gitlab/ui';
+import { GlSprintf } from '@gitlab/ui';
 import SecurityConfigurationApp from 'ee/security_configuration/components/app.vue';
+
+// Need to stub the GlTable so that the props are recognized as props rather than attributes.
+const tableStub = {
+  props: ['items', 'stacked'],
+  render: () => {},
+};
 
 describe('Security Configuration App', () => {
   let wrapper;
   const createComponent = (props = {}) => {
     wrapper = shallowMount(SecurityConfigurationApp, {
+      stubs: { GlSprintf, GlTable: tableStub },
       propsData: {
         features: [],
         autoDevopsEnabled: false,
@@ -30,21 +36,10 @@ describe('Security Configuration App', () => {
       link: `link-feature-${i}`,
     }));
 
-  const getHelpLink = () => wrapper.find('header').find(GlLink);
-  const getNotification = () => wrapper.find({ ref: 'callout' });
-  const getPipelinesLink = () => getNotification().find('a');
-  const getFeaturesTable = () => wrapper.find({ ref: 'featuresTable' });
-  const getFeatureConfigStatus = () => wrapper.find({ ref: 'featureConfigStatus' });
+  const getPipelinesLink = () => wrapper.find({ ref: 'pipelinesLink' });
+  const getFeaturesTable = () => wrapper.find({ ref: 'securityControlTable' });
 
   describe('header', () => {
-    it('displays a link to the given help page', () => {
-      const helpPagePath = 'http://foo';
-
-      createComponent({ helpPagePath });
-
-      expect(getHelpLink().attributes('href')).toBe(helpPagePath);
-    });
-
     it.each`
       autoDevopsEnabled | expectedUrl
       ${true}           | ${'http://autoDevopsHelpPagePath'}
@@ -55,40 +50,31 @@ describe('Security Configuration App', () => {
         createComponent({ autoDevopsEnabled });
 
         expect(getPipelinesLink().attributes('href')).toBe(expectedUrl);
-        expect(getPipelinesLink().attributes('rel')).toBe('noopener');
+        expect(getPipelinesLink().attributes('target')).toBe('_blank');
       },
     );
   });
 
   describe('features table', () => {
-    it('displays a row for each given feature', () => {
+    it('passes the expected data to the GlTable', () => {
       const features = generateFeatures(5);
 
       createComponent({ features });
 
-      expect(wrapper.findAll({ ref: 'featureRow' })).toHaveLength(5);
-    });
-
-    it('displays a given feature', () => {
-      const features = generateFeatures(1);
-
-      createComponent({ features });
-
-      expect(getFeaturesTable().element).toMatchSnapshot();
+      expect(getFeaturesTable().props('items')).toHaveLength(5);
+      expect(getFeaturesTable().props('stacked')).toBe('md');
     });
 
     it.each`
       configured | statusText
-      ${true}    | ${'Configured'}
-      ${false}   | ${'Not yet configured'}
+      ${true}    | ${'Enabled'}
+      ${false}   | ${'Not yet enabled'}
     `(
       `displays "$statusText" if the given feature's configuration status is: "$configured"`,
       ({ configured, statusText }) => {
-        const features = [{ configured }];
+        createComponent();
 
-        createComponent({ features });
-
-        expect(getFeatureConfigStatus().text()).toBe(statusText);
+        expect(wrapper.vm.getStatusText(configured)).toBe(statusText);
       },
     );
   });
