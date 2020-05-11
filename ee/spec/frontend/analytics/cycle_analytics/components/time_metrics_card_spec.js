3530d4d53db80129dd905dc8201579f0dee2fb26
Ezekiel Kigbo <ekigbo@gitlab.com>
2020-05-11 12:04:56 +1000

    [skip ci] wip - Adds additional specs for time summary data

--- ee/spec/frontend/analytics/cycle_analytics/components/time_metrics_card_spec.js
+++ ee/spec/frontend/analytics/cycle_analytics/components/time_metrics_card_spec.js
@@ -1,20 +1,25 @@
 import { mount } from '@vue/test-utils';
+import createFlash from '~/flash';
 import TimeMetricsCard from 'ee/analytics/cycle_analytics/components/time_metrics_card.vue';
-import { group } from '../mock_data';
+import { group, timeMetricsData } from '../mock_data';
+import Api from 'ee/api';
 
 describe('TimeMetricsCard', () => {
   const { full_path: groupPath } = group;
   let wrapper;
 
-  const createComponent = () => {
+  const createComponent = (additionalParams = {}) => {
     return mount(TimeMetricsCard, {
       propsData: {
         groupPath,
+        additionalParams,
       },
     });
   };
 
   beforeEach(() => {
+    jest.spyOn(Api, 'cycleAnalyticsTimeSummaryData').mockResolvedValue({ data: timeMetricsData });
+
     wrapper = createComponent();
   });
 
@@ -24,5 +29,41 @@ describe('TimeMetricsCard', () => {
 
   it('matches the snapshot', () => {
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('fetches the time metrics data', () => {
+    expect(Api.cycleAnalyticsTimeSummaryData).toHaveBeenCalledWith(groupPath, {});
+  });
+
+  describe('with a failing request', () => {
+    beforeEach(() => {
+      jest.spyOn(Api, 'cycleAnalyticsTimeSummaryData').mockRejectedValue();
+
+      wrapper = createComponent();
+    });
+
+    it('should render an error message', () => {
+      expect(createFlash).toHaveBeenCalledWith(
+        'There was an error while fetching value stream analytics time metrics data.',
+      );
+    });
+  });
+
+  describe('with additional params', () => {
+    beforeEach(() => {
+      wrapper = createComponent({
+        'project_ids[]': [1],
+        created_after: '2020-01-01',
+        created_before: '2020-02-01',
+      });
+    });
+
+    it('sends additional parameters as query paremeters', () => {
+      expect(Api.cycleAnalyticsTimeSummaryData).toHaveBeenCalledWith(groupPath, {
+        'project_ids[]': [1],
+        created_after: '2020-01-01',
+        created_before: '2020-02-01',
+      });
+    });
   });
 });
