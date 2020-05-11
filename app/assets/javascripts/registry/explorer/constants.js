d034e0078e3b21a571ecd23eb41e3758b87435e7
Nicolò Maria Mezzopera <nmezzopera@gitlab.com>
2020-05-11 15:16:25 +0000

    Apply suggestion to app/assets/javascripts/registry/explorer/constants.js

--- app/assets/javascripts/registry/explorer/constants.js
+++ app/assets/javascripts/registry/explorer/constants.js
@@ -41,7 +41,7 @@ export const IMAGE_REPOSITORY_LIST_LABEL = s__('ContainerRegistry|Image Reposito
 
 export const SEARCH_PLACEHOLDER_TEXT = s__('ContainerRegistry|Filter by name');
 
-export const EMPTY_RESULT_TITLE = s__('ContainerRegistry|Sorry, your filter produced no results');
+export const EMPTY_RESULT_TITLE = s__('ContainerRegistry|Sorry, your filter produced no results.');
 export const EMPTY_RESULT_MESSAGE = s__(
   'ContainerRegistry|To widen your search, change or remove the filters above',
 );
