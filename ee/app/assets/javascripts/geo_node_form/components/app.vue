477f35b9ee06d97be8e30400c4b036e8bfe944ae
Zack Cuddy <zcuddy@gitlab.com>
2020-05-12 14:50:57 -0500

    Better visualize type of node on Geo Form
    This MR adds a pill at the top of the Geo
    Form that identifies what type of node
    you are creating/updating.
    
    Also, creating Primary nodes from the UI
    is no longer supported.  This change
    also removes the redundant Primary
    node checkbox.

--- ee/app/assets/javascripts/geo_node_form/components/app.vue
+++ ee/app/assets/javascripts/geo_node_form/components/app.vue
@@ -26,13 +26,24 @@ export default {
     pageTitle() {
       return this.node ? __('Edit Geo Node') : __('New Geo Node');
     },
+    pillDetails() {
+      return {
+        color: this.node && this.node.primary ? 'bg-primary-400 text-white' : 'bg-secondary-100',
+        label: this.node && this.node.primary ? __('Primary') : __('Secondary'),
+      };
+    },
   },
 };
 </script>
 
 <template>
   <article class="geo-node-form-container">
-    <h3 class="page-title">{{ pageTitle }}</h3>
+    <div class="d-flex align-items-center">
+      <h3 class="page-title">{{ pageTitle }}</h3>
+      <span class="rounded-pill gl-font-sm px-2 py-1 ml-2" :class="pillDetails.color">{{
+        pillDetails.label
+      }}</span>
+    </div>
     <geo-node-form v-bind="$props" />
   </article>
 </template>
--- ee/spec/frontend/geo_node_form/components/app_spec.js
+++ ee/spec/frontend/geo_node_form/components/app_spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import GeoNodeFormApp from 'ee/geo_node_form/components/app.vue';
 import GeoNodeForm from 'ee/geo_node_form/components/geo_node_form.vue';
-import { MOCK_SELECTIVE_SYNC_TYPES, MOCK_SYNC_SHARDS, MOCK_NODE } from '../mock_data';
+import { MOCK_SELECTIVE_SYNC_TYPES, MOCK_SYNC_SHARDS } from '../mock_data';
 
 describe('GeoNodeFormApp', () => {
   let wrapper;
@@ -23,6 +23,7 @@ describe('GeoNodeFormApp', () => {
   });
 
   const findGeoNodeFormTitle = () => wrapper.find('.page-title');
+  const findGeoNodeFormPill = () => wrapper.find('.rounded-pill');
   const findGeoForm = () => wrapper.find(GeoNodeForm);
 
   describe('render', () => {
@@ -30,35 +31,38 @@ describe('GeoNodeFormApp', () => {
       createComponent();
     });
 
-    it('the Geo Node Form Title', () => {
-      expect(findGeoNodeFormTitle().exists()).toBe(true);
+    describe.each`
+      formType                     | node                  | title              | pillTitle      | pillClass
+      ${'create a secondary node'} | ${null}               | ${'New Geo Node'}  | ${'Secondary'} | ${'bg-secondary-100'}
+      ${'update a secondary node'} | ${{ primary: false }} | ${'Edit Geo Node'} | ${'Secondary'} | ${'bg-secondary-100'}
+      ${'update a primary node'}   | ${{ primary: true }}  | ${'Edit Geo Node'} | ${'Primary'}   | ${'bg-primary-400 text-white'}
+    `(`form header`, ({ formType, node, title, pillTitle, pillClass }) => {
+      describe(`when node form is to ${formType}`, () => {
+        beforeEach(() => {
+          propsData.node = node;
+          createComponent();
+        });
+
+        it(`sets the node form title to ${title}`, () => {
+          expect(findGeoNodeFormTitle().text()).toBe(title);
+        });
+
+        it(`sets the node form pill title to ${pillTitle}`, () => {
+          expect(findGeoNodeFormPill().text()).toBe(pillTitle);
+        });
+
+        it(`sets the node form pill classes to contain ${pillClass}`, () => {
+          expect(
+            findGeoNodeFormPill()
+              .classes()
+              .join(' '),
+          ).toContain(pillClass);
+        });
+      });
     });
 
     it('the Geo Node Form', () => {
       expect(findGeoForm().exists()).toBe(true);
-    });
-  });
-
-  describe('Geo Node Form Title', () => {
-    describe('when props.node is undefined', () => {
-      beforeEach(() => {
-        createComponent();
-      });
-
-      it('sets title to `New Geo Node`', () => {
-        expect(findGeoNodeFormTitle().text()).toBe('New Geo Node');
-      });
-    });
-
-    describe('when props.node is set', () => {
-      beforeEach(() => {
-        propsData.node = MOCK_NODE;
-        createComponent();
-      });
-
-      it('sets title to `Edit Geo Node`', () => {
-        expect(findGeoNodeFormTitle().text()).toBe('Edit Geo Node');
-      });
     });
   });
 });
