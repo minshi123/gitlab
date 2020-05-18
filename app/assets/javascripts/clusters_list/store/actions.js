f85d08407f5f836443175e24cf95e5dc78791de3
Emily Ring <ering@gitlab.com>
2020-05-18 09:51:22 -0400

    Added retry limit

--- app/assets/javascripts/clusters_list/store/actions.js
+++ app/assets/javascripts/clusters_list/store/actions.js
@@ -2,14 +2,16 @@ import Poll from '~/lib/utils/poll';
 import axios from '~/lib/utils/axios_utils';
 import flash from '~/flash';
 import { __ } from '~/locale';
+import { MAX_REQUESTS } from '../constants';
 import * as types from './mutation_types';
 
-// Nodes are part of a reactive cache which may take mutliple requests to finish querying
-const nodesQueried = clusters => {
+const allNodesPresent = clusters => {
   return clusters.length === clusters.filter(cluster => cluster.nodes != null).length;
 };
 
 export const fetchClusters = ({ state, commit }) => {
+  let retryCount = 0;
+
   const poll = new Poll({
     resource: {
       fetchClusters: endpoint => axios.get(endpoint),
@@ -17,16 +19,22 @@ export const fetchClusters = ({ state, commit }) => {
     data: state.endpoint,
     method: 'fetchClusters',
     successCallback: ({ data }) => {
+      retryCount += 1;
+
       if (data.clusters) {
         commit(types.SET_CLUSTERS_DATA, data);
         commit(types.SET_LOADING_STATE, false);
 
-        if (nodesQueried(data.clusters)) {
+        if (allNodesPresent(data.clusters) || retryCount > MAX_REQUESTS) {
           poll.stop();
         }
       }
     },
-    errorCallback: () => flash(__('An error occurred while loading clusters')),
+    errorCallback: () => {
+      commit(types.SET_LOADING_STATE, false);
+      flash(__('An error occurred while loading clusters'));
+      poll.stop();
+    },
   });
 
   poll.makeRequest();
