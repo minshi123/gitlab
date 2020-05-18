400693ab2fcec2d571a827cc5bd16cd4d25192c9
Terri Chu <tchu@gitlab.com>
2020-05-18 15:11:41 -0400

    Update search counter spec & add changelog

--- spec/lib/gitlab/usage_data_counters/search_counter_spec.rb
+++ spec/lib/gitlab/usage_data_counters/search_counter_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 describe Gitlab::UsageDataCounters::SearchCounter, :clean_gitlab_redis_shared_state do
+  it_behaves_like 'a redis usage counter', 'Global Search', 'all_searches_count'
+
+  it_behaves_like 'a redis usage counter with totals', :global_search, all_searches_count: 3
+
   it 'increments counter and return the total count' do
     expect(described_class.total_navbar_searches_count).to eq(0)
 
