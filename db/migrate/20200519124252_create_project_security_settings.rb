da63790dd7e3416410e3066e3f1df7893e8fb90c
Tetiana Chupryna <tchupryna@gitlab.com>
2020-05-19 16:02:54 +0300

    Add Project Security setting table

---
+++ db/migrate/20200519124252_create_project_security_settings.rb
@@ -0,0 +1,23 @@
+class CreateProjectSecuritySettings < ActiveRecord::Migration[6.0]
+  include Gitlab::Database::MigrationHelpers
+
+  def up
+    with_lock_retries do
+      create_table :project_security_settings do |t|
+        t.references :project, index: true, null: false, foreign_key: { on_delete: :cascade }
+        t.boolean :auto_fix_container_scanning, default: true
+        t.boolean :auto_fix_dast, default: true
+        t.boolean :auto_fix_dependency_scanning, default: true
+        t.boolean :auto_fix_sast, default: true
+
+        t.timestamps
+      end
+    end
+  end
+
+  def down
+    with_lock_retries do
+      drop_table :project_security_settings
+    end
+  end
+end
