5e92c4679abb8d97515f702dadb0e098abf6652d
Tetiana Chupryna <tchupryna@gitlab.com>
2020-05-20 20:59:46 +0300

    Save auto-fix setting

--- ee/app/controllers/projects/security/configuration_controller.rb
+++ ee/app/controllers/projects/security/configuration_controller.rb
@@ -7,12 +7,30 @@ module Projects
 
       alias_method :vulnerable, :project
 
+      before_action :check_feature_flag
+
       before_action only: [:show] do
         push_frontend_feature_flag(:security_auto_fix, project, default_enabled: false)
       end
 
       def show
         @configuration = ConfigurationPresenter.new(project)
+      end
+
+      def auto_fix
+        setting = project.security_setting || project.create_security_setting
+        feature = params[:feature]
+        enabled = params[:enabled]
+
+        setting.update("auto_fix_#{feature}", enabled)
+
+        render_ok
+      end
+
+      private
+
+      def check_feature_flag
+        return render_404 if Feature.disabled?(:security_auto_fix, project, default_enabled: false)
       end
     end
   end
--- ee/spec/controllers/projects/security/configuration_controller_spec.rb
+++ ee/spec/controllers/projects/security/configuration_controller_spec.rb
@@ -3,66 +3,94 @@
 require 'spec_helper'
 
 describe Projects::Security::ConfigurationController do
-  let(:group)   { create(:group) }
+  let(:group) { create(:group) }
   let(:project) { create(:project, :repository, namespace: group) }
 
-  subject { get :show, params: { namespace_id: project.namespace, project_id: project } }
-
-  it_behaves_like SecurityDashboardsPermissions do
-    let(:vulnerable) { project }
-
-    let(:security_dashboard_action) do
-      subject
-    end
-  end
-
   describe 'GET #show' do
-    let(:user) { create(:user) }
+    subject { get :show, params: { namespace_id: project.namespace, project_id: project } }
 
-    render_views
+    it_behaves_like SecurityDashboardsPermissions do
+      let(:vulnerable) { project }
 
-    before do
-      stub_licensed_features(security_dashboard: true)
-
-      group.add_developer(user)
-      sign_in(user)
+      let(:security_dashboard_action) do
+        subject
+      end
     end
 
-    it "renders data on the project's security configuration" do
-      subject
+    context 'with user' do
+      let(:user) { create(:user) }
 
-      expect(response).to have_gitlab_http_status(:ok)
-      expect(response).to render_template(:show)
-      expect(response.body).to have_css(
-        'div#js-security-configuration'\
-          "[data-auto-devops-help-page-path=\"#{help_page_path('topics/autodevops/index')}\"]"\
-          "[data-help-page-path=\"#{help_page_path('user/application_security/index')}\"]"\
-          "[data-latest-pipeline-path=\"#{help_page_path('ci/pipelines')}\"]"
-      )
-    end
+      render_views
 
-    context 'when the latest pipeline used Auto DevOps' do
-      let!(:pipeline) do
-        create(
-          :ci_pipeline,
-          :auto_devops_source,
-          project: project,
-          ref: project.default_branch,
-          sha: project.commit.sha
-        )
+      before do
+        stub_licensed_features(security_dashboard: true)
+
+        group.add_developer(user)
+        sign_in(user)
       end
 
-      it 'reports that Auto DevOps is enabled' do
+      it "renders data on the project's security configuration" do
         subject
 
         expect(response).to have_gitlab_http_status(:ok)
+        expect(response).to render_template(:show)
         expect(response.body).to have_css(
-          'div#js-security-configuration'\
+                                   'div#js-security-configuration'\
+          "[data-auto-devops-help-page-path=\"#{help_page_path('topics/autodevops/index')}\"]"\
+          "[data-help-page-path=\"#{help_page_path('user/application_security/index')}\"]"\
+          "[data-latest-pipeline-path=\"#{help_page_path('ci/pipelines')}\"]"
+                                 )
+      end
+
+      context 'when the latest pipeline used Auto DevOps' do
+        let!(:pipeline) do
+          create(
+            :ci_pipeline,
+            :auto_devops_source,
+            project: project,
+            ref: project.default_branch,
+            sha: project.commit.sha
+          )
+        end
+
+        it 'reports that Auto DevOps is enabled' do
+          subject
+
+          expect(response).to have_gitlab_http_status(:ok)
+          expect(response.body).to have_css(
+                                     'div#js-security-configuration'\
             '[data-auto-devops-enabled]'\
             "[data-auto-devops-help-page-path=\"#{help_page_path('topics/autodevops/index')}\"]"\
             "[data-help-page-path=\"#{help_page_path('user/application_security/index')}\"]"\
             "[data-latest-pipeline-path=\"#{project_pipeline_path(project, pipeline)}\"]"
-        )
+                                   )
+        end
+      end
+    end
+  end
+
+  describe 'POST #auto_fix' do
+    subject { post :auto_fix, params: params }
+
+    let_it_be(:user) { create(:user) }
+
+    # context 'with feature enabled' do
+    #
+    # end
+
+    context 'with feature disabled' do
+      before do
+        stub_licensed_features(security_dashboard: true)
+
+        project.add_developer(user)
+        sign_in(user)
+      end
+
+
+      let(:params) {}
+
+      it 'returns 404 error' do
+        expect(response).to have_gitlab_http_status(:not_found)
       end
     end
   end
