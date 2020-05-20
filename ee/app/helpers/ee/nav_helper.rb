d23d39d12cad3fea48df14a93f522d46770ac60b
Simon Knox <simon@gitlab.com>
2020-05-20 12:34:23 +1000

    Add navbar_spec spec
    Change other feature specs to beta

--- ee/app/helpers/ee/nav_helper.rb
+++ ee/app/helpers/ee/nav_helper.rb
@@ -23,7 +23,7 @@ module EE
     override :group_issues_sub_menu_items
     def group_issues_sub_menu_items
       controllers = %w(issues_analytics#show)
-      if @group.feature_available?(:iterations)
+      if @group.beta_feature_available?(:iterations)
         controllers = %w(iterations#index)
       end
       super.concat(controllers)
