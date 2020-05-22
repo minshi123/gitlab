cebae79b3d5d6e3366b0331774a964f99c979a06
Etienne Baqué <ebaque@gitlab.com>
2020-05-22 17:19:13 -0400

    Added group_deploy_key_groups table
    Added migration file and db structure updates.

---
+++ db/migrate/20200522205606_create_group_deploy_keys_group.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreateGroupDeployKeysGroup < ActiveRecord::Migration[6.0]
+  DOWNTIME = false
+
+  def change
+    create_table :group_deploy_keys_groups do |t|
+      t.timestamps_with_timezone
+      t.references :group, index: false, null: false, foreign_key: { to_table: :namespaces, on_delete: :cascade }
+      t.references :group_deploy_key, null: false, foreign_key: { on_delete: :cascade }
+
+      t.index [:group_id, :group_deploy_key_id], unique: true, name: 'index_group_deploy_keys_group_on_group_deploy_key_and_group_ids'
+    end
+  end
+end
