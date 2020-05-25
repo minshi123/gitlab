72387aa180933f81f052a8a8bb82ae4e2e022c34
Dylan Griffith <dyl.griffith@gmail.com>
2020-05-25 17:04:55 +1000

    Show Elasticsearch in performance bar

---
+++ lib/peek/views/elasticsearch_detailed.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Peek
+  module Views
+    class ElasticsearchDetailed < DetailedView
+      DEFAULT_THRESHOLDS = {
+        calls: 5,
+        duration: 1000,
+        individual_call: 1000
+      }.freeze
+
+      THRESHOLDS = {
+        production: {
+          calls: 5,
+          duration: 1000,
+          individual_call: 1000
+        }
+      }.freeze
+
+      def key
+        'es'
+      end
+
+      def self.thresholds
+        @thresholds ||= THRESHOLDS.fetch(Rails.env.to_sym, DEFAULT_THRESHOLDS)
+      end
+
+      private
+
+      def duration
+        ::Gitlab::Instrumentation::ElasticsearchTransport.query_time * 1000
+      end
+
+      def calls
+        ::Gitlab::Instrumentation::ElasticsearchTransport.get_request_count
+      end
+
+      def call_details
+        ::Gitlab::Instrumentation::ElasticsearchTransport.detail_store
+      end
+    end
+  end
+end
+
