691d7e5da0f21dca061fdba2c79d709c956435ff
Dave Pisek <dpisek@gitlab.com>
2020-05-26 14:28:10 +1000

    Add tooltip to unknown-status on severity badge
    This commit adds a tooltip, which explains the "unknown" status, to
    the severity-badge Vue component.

--- ee/app/assets/javascripts/vue_shared/security_reports/components/severity_badge.vue
+++ ee/app/assets/javascripts/vue_shared/security_reports/components/severity_badge.vue
@@ -1,6 +1,7 @@
 <script>
 import { SEVERITY_LEVELS } from 'ee/security_dashboard/store/constants';
-import { GlIcon } from '@gitlab/ui';
+import { GlIcon, GlTooltipDirective } from '@gitlab/ui';
+import { __ } from '~/locale';
 
 export const CLASS_NAME_MAP = {
   critical: 'text-danger-800',
@@ -11,10 +12,19 @@ export const CLASS_NAME_MAP = {
   unknown: 'text-secondary-400',
 };
 
+export const HELP_TEXT_MAP = {
+  unknown: __(
+    `The rating "unknown" indicates that the underlying scanner doesn’t contain or provide a severity rating.`,
+  ),
+};
+
 export default {
   name: 'SeverityBadge',
   components: {
     GlIcon,
+  },
+  directives: {
+    GlTooltip: GlTooltipDirective,
   },
   props: {
     severity: {
@@ -38,13 +48,23 @@ export default {
     severityTitle() {
       return SEVERITY_LEVELS[this.severityKey] || this.severity;
     },
+    toolTipTitle() {
+      return HELP_TEXT_MAP[this.severityKey] || '';
+    },
   },
 };
 </script>
 
 <template>
   <div v-if="hasSeverityBadge" class="severity-badge text-left text-nowrap gl-text-gray-900">
-    <span :class="className"><gl-icon :name="iconName" :size="12" class="append-right-8"/></span
-    >{{ severityTitle }}
+    <span :class="className"
+      ><gl-icon
+        v-gl-tooltip
+        :title="toolTipTitle"
+        :name="iconName"
+        :size="12"
+        class="append-right-8"
+    /></span>
+    {{ severityTitle }}
   </div>
 </template>
