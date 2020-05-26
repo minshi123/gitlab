4ebb608e0487dd7bff83429ff0fe50de19e46750
Rayana Verissimo <rverissimo@gitlab.com>
2020-05-26 12:35:21 +0200

    Fix environment dashboard card inner content sizing by updating modifier classes

--- ee/app/assets/javascripts/environments_dashboard/components/dashboard/environment.vue
+++ ee/app/assets/javascripts/environments_dashboard/components/dashboard/environment.vue
@@ -118,7 +118,7 @@ export default {
 };
 </script>
 <template>
-  <div class="dashboard-card card border-0">
+  <div class="environment-dashboard-card dashboard-card card border-0">
     <environment-header
       :environment="environment"
       :has-pipeline-failed="hasPipelineFailed"
@@ -127,7 +127,7 @@ export default {
 
     <div :class="cardClasses" class="dashboard-card-body card-body">
       <div v-if="lastDeployment" class="row">
-        <div class="col-1 align-self-center">
+        <div class="col-1 align-self-center pl-3 pr-3">
           <user-avatar-link
             v-if="user"
             :link-href="user.path"
@@ -162,7 +162,7 @@ export default {
         </div>
 
         <div
-          class="col-sm-4 mt-0 pl-5 pl-sm-0 offset-1 offset-sm-0 text-sm-right align-self-center col-12 d-sm-block"
+          class="col-sm-3 mt-0 pl-6 pr-0 pl-sm-0 offset-1 offset-sm-0 text-sm-right align-self-center col-12 d-sm-block"
         >
           <time-ago
             v-if="shouldShowTimeAgo"
