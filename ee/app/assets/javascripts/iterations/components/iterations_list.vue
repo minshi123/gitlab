416952bcb06d676fd333b785680c3cef405c7cc5
Simon Knox <simon@gitlab.com>
2020-05-28 16:31:49 +1000

    Nicer looking date formatting for list
    Also fix up spacing

--- ee/app/assets/javascripts/iterations/components/iterations_list.vue
+++ ee/app/assets/javascripts/iterations/components/iterations_list.vue
@@ -1,9 +1,16 @@
 <script>
 import { GlLink } from '@gitlab/ui';
+import dateFormat from 'dateformat';
 
 export default {
   components: {
     GlLink,
+  },
+  filters: {
+    date: value => {
+      const date = new Date(value);
+      return dateFormat(date, 'mmm d, yyyy', true);
+    },
   },
   props: {
     iterations: {
@@ -19,10 +26,14 @@ export default {
   <div class="milestones mt-0">
     <ul v-if="iterations.length > 0" class="content-list">
       <li v-for="iteration in iterations" :key="iteration.id" class="milestone">
-        <gl-link :href="iteration.webPath"
-          ><strong>{{ iteration.title }}</strong></gl-link
-        >
-        <p class="text-secondary">{{ iteration.startDate }} - {{ iteration.dueDate }}</p>
+        <div class="gl-mb-3">
+          <gl-link :href="iteration.webPath"
+            ><strong>{{ iteration.title }}</strong></gl-link
+          >
+        </div>
+        <div class="text-secondary gl-mb-3">
+          {{ iteration.startDate | date }}–{{ iteration.dueDate | date }}
+        </div>
       </li>
     </ul>
     <div v-else class="nothing-here-block">
