cef1c48cafaa72ea9645aa8e93d47869bbc87697
Simon Knox <simon@gitlab.com>
2020-06-05 08:54:31 +1000

    Check for iterations in list

--- ee/spec/features/groups/iterations/iterations_list_spec.rb
+++ ee/spec/features/groups/iterations/iterations_list_spec.rb
@@ -6,8 +6,9 @@ describe 'Iterations list', :js do
   let(:now) { Time.now }
   let_it_be(:group) { create(:group) }
   let_it_be(:user) { create(:user) }
-  let!(:started_group_iteration) { create(:iteration, :skip_future_date_validation, group: group, title: 'one test', start_date: now - 1.day, due_date: now) }
-  let!(:upcoming_group_iteration) { create(:iteration, group: group, start_date: now + 1.day, due_date: now + 2.days) }
+  let!(:started_iteration) { create(:iteration, :skip_future_date_validation, group: group, start_date: now - 1.day, due_date: now) }
+  let!(:upcoming_iteration) { create(:iteration, group: group, start_date: now + 1.day, due_date: now + 2.days) }
+  let!(:closed_iteration) { create(:closed_iteration, :skip_future_date_validation, group: group, start_date: now - 3.days, due_date: now - 2.days) }
 
   context 'as guest' do
     before do
@@ -16,6 +17,24 @@ describe 'Iterations list', :js do
 
     it 'hides New iteration button' do
       expect(page).not_to have_link('New iteration', href: new_group_iteration_path(group))
+    end
+
+    it 'shows iterations on each tab' do
+      expect(page).to have_link(started_iteration.title)
+      expect(page).to have_link(upcoming_iteration.title)
+      expect(page).not_to have_link(closed_iteration.title)
+
+      click_link('Closed')
+
+      expect(page).to have_link(closed_iteration.title)
+      expect(page).not_to have_link(started_iteration.title)
+      expect(page).not_to have_link(upcoming_iteration.title)
+
+      click_link('All')
+
+      expect(page).to have_link(started_iteration.title)
+      expect(page).to have_link(upcoming_iteration.title)
+      expect(page).to have_link(closed_iteration.title)
     end
   end
 
