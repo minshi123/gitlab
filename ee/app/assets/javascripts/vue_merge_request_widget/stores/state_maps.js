12a7a85d02c9d929efb2feafe7045e20eb0e01df
Fernando <farias@gitlab.com>
2020-06-07 21:40:53 -0500

    Add missing stateKey

--- ee/app/assets/javascripts/vue_merge_request_widget/stores/state_maps.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/stores/state_maps.js
@@ -1,7 +1,11 @@
 import stateMaps from '~/vue_merge_request_widget/stores/state_maps';
 
 stateMaps.stateToComponentMap.geoSecondaryNode = 'mr-widget-geo-secondary-node';
-stateMaps.stateToComponentMap.polivyViolation = 'mr-widget-policy-violation';
+stateMaps.stateToComponentMap.policyViolation = 'mr-widget-policy-violation';
+
+export const stateKey = {
+  policyViolation: 'policyViolation'
+};
 
 export default {
   stateToComponentMap: stateMaps.stateToComponentMap,
