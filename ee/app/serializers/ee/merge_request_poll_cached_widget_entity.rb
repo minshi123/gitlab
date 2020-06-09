e1aa03bd5d7730227f13a7514883601e597ef562
Tetiana Chupryna <tchupryna@gitlab.com>
2020-06-09 16:58:56 +0300

    Add placeholder for policy denied

--- ee/app/serializers/ee/merge_request_poll_cached_widget_entity.rb
+++ ee/app/serializers/ee/merge_request_poll_cached_widget_entity.rb
@@ -8,6 +8,10 @@ module EE
       expose :merge_train_when_pipeline_succeeds_docs_path do |merge_request|
         presenter(merge_request).merge_train_when_pipeline_succeeds_docs_path
       end
+
+      expose :policy_violation do |_|
+        Feature.enabled?(:license_compliance_denies_mr, merge_request.project, default_enabled: false)
+      end
     end
   end
 end
