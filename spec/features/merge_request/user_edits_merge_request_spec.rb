04617b5ca471c8771e94847fc2172b143437881f
Samantha Ming <sming@gitlab.com>
2020-06-25 18:29:32 -0700

    Attempt to write test for recovery

--- spec/features/merge_request/user_edits_merge_request_spec.rb
+++ spec/features/merge_request/user_edits_merge_request_spec.rb
@@ -16,6 +16,36 @@ RSpec.describe 'User edits a merge request', :js do
     visit(edit_project_merge_request_path(project, merge_request))
   end
 
+  context "Squash commits"  do
+    # If the MR selection is checked to begin with
+    # User change settings to "Do not allow" (unchecked)
+    # Go to edit MR and save it
+    # Change project settings to be "Allow" (uncheck)
+    # Go to edit page, squash commit should be checked
+    it 'recovers MR setting of squash true after "Do not allow" is saved' do
+      merge_request.update!(squash: true)
+      project.project_setting.update!(squash_option: 'never')
+      visit(edit_project_merge_request_path(project, merge_request))
+      click_button('Save changes')
+
+      project.project_setting.update!(squash_option: 'default_off')
+      visit(edit_project_merge_request_path(project, merge_request))
+
+      checkbox = find("#merge_request_squash")
+      expect(checkbox.selected?).to be(true)
+    end
+
+    # If the MR selection is unchecked to begin with
+    # User change settings to "Required" (checked)
+    # Go to edit MR and save it
+    # Change project settings to be "Encourage" (check)
+    # Go to edit page, squash commit will recover its original MR select and will be unchecked
+
+    # it 'recovers MR setting of squash false after "Required" is saved' do
+
+    # end
+  end
+
   it 'changes the target branch' do
     expect(page).to have_content('From master into feature')
 
