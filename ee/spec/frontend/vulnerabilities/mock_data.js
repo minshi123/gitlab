b5cb449e51a0c60b8e2a21d5d9798eb7000c4efc
Alexander Turinske <aturinske@gitlab.com>
2020-06-26 18:01:48 -0600

    Add line number to SAST and Secret Detection
    - based on the what the location object has, create the location
      line for the vulnerability
    - modify/add tests

--- ee/spec/frontend/vulnerabilities/mock_data.js
+++ ee/spec/frontend/vulnerabilities/mock_data.js
@@ -1,7 +1,7 @@
 export const generateVulnerabilities = () => [
   {
     id: 'id_0',
-    title: 'Vulnerability 1',
+    title: 'Vulnerability 0',
     severity: 'critical',
     state: 'dismissed',
     location: {
@@ -14,14 +14,28 @@ export const generateVulnerabilities = () => [
   },
   {
     id: 'id_1',
-    title: 'Vulnerability 2',
+    title: 'Vulnerability 1',
     severity: 'high',
     state: 'opened',
     location: {
       file: 'src/main/java/com/gitlab/security_products/tests/App.java',
+      startLine: '1337',
     },
     project: {
       nameWithNamespace: 'Administrator / Vulnerability reports',
+    },
+  },
+  {
+    id: 'id_2',
+    title: 'Vulnerability 2',
+    severity: 'high',
+    state: 'opened',
+    reportType: 'CUSTOM_SCANNER_WITHOUT_TRANSLATION',
+    location: {
+      file: 'src/main/java/com/gitlab/security_products/tests/App.java',
+    },
+    project: {
+      nameWithNamespace: 'Mixed Vulnerabilities / Dependency List Test 01',
     },
   },
 ];
