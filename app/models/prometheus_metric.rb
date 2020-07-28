f7559e59258f9537f9c0164dba98d414434b046d
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-07-28 15:18:44 +0200

    Move PrometheusMetricEnums to Enums::PrometheusMetric
    Move enums related modules to a separate directory

--- app/models/prometheus_metric.rb
+++ app/models/prometheus_metric.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class PrometheusMetric < ApplicationRecord
+  include Enums::PrometheusMetric
+
   belongs_to :project, validate: true, inverse_of: :prometheus_metrics
   has_many :prometheus_alerts, inverse_of: :prometheus_metric
-
-  enum group: PrometheusMetricEnums.groups
 
   validates :title, presence: true
   validates :query, presence: true
@@ -72,6 +72,6 @@ class PrometheusMetric < ApplicationRecord
   private
 
   def group_details(group)
-    PrometheusMetricEnums.group_details.fetch(group.to_sym)
+    Enums::PrometheusMetric.group_details.fetch(group.to_sym)
   end
 end
