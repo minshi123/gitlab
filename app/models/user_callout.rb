16c4792c3754e6baefe2d63257d35e639c1020b2
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-07-28 11:09:13 +0200

    Move UserCalloutEnums to Enums::UserCallout
    Move enums related modules into a separate directory.

--- app/models/user_callout.rb
+++ app/models/user_callout.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class UserCallout < ApplicationRecord
-  belongs_to :user
+  include Enums::UserCallout
 
-  # We use `UserCalloutEnums.feature_names` here so that EE can more easily
-  # extend this `Hash` with new values.
-  enum feature_name: ::UserCalloutEnums.feature_names
+  belongs_to :user
 
   validates :user, presence: true
   validates :feature_name,
