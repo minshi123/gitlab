4515fb9f915610709ddaa5976e1da5095dfb7f4d
Samantha Ming <sming@gitlab.com>
2020-08-24 17:48:50 -0700

    WIP

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -55,6 +55,39 @@ module FormHelper
     dropdown_data
   end
 
+  def reviewers_dropdown_options(issuable_type)
+    dropdown_data = {
+      toggle_class: 'js-user-search js-assignee-search js-multiselect js-save-user-data',
+      title: 'Select reviewer asdf',
+      filter: true,
+      dropdown_class: 'dropdown-menu-user dropdown-menu-selectable dropdown-menu-reviewer',
+      placeholder: _('Search users'),
+      data: {
+        first_user: current_user&.username,
+        null_user: true,
+        current_user: true,
+        project_id: (@target_project || @project)&.id,
+        field_name: "#{issuable_type}[assignee_ids][]",
+        default_label: 'Unassigned',
+        'max-select': 1,
+        'dropdown-header': 'Reviewer',
+        multi_select: true,
+        'input-meta': 'name',
+        'always-show-selectbox': true,
+        current_user_info: UserSerializer.new.represent(current_user)
+      }
+    }
+
+    type = issuable_type.to_s
+
+    if type == 'issue' && issue_supports_multiple_reviewers? ||
+        type == 'merge_request' && merge_request_supports_multiple_reviewers?
+      dropdown_data = multiple_reviewers_dropdown_options(dropdown_data)
+    end
+
+    dropdown_data
+  end
+
   # Overwritten
   def issue_supports_multiple_assignees?
     false
@@ -65,6 +98,16 @@ module FormHelper
     false
   end
 
+  # Overwritten
+  def issue_supports_multiple_reviewers?
+    false
+  end
+
+  # Overwritten
+  def merge_request_supports_multiple_reviewers?
+    false
+  end
+
   private
 
   def multiple_assignees_dropdown_options(options)
@@ -72,6 +115,16 @@ module FormHelper
 
     new_options[:title] = 'Select assignee(s)'
     new_options[:data][:'dropdown-header'] = 'Assignee(s)'
+    new_options[:data].delete(:'max-select')
+
+    new_options
+  end
+
+  def multiple_reviewers_dropdown_options(options)
+    new_options = options.dup
+
+    new_options[:title] = 'Select reviewer(s) 123'
+    new_options[:data][:'dropdown-header'] = 'Reviewer(s) 456'
     new_options[:data].delete(:'max-select')
 
     new_options
