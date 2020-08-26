5779dec79de0817dcf4ceee251dff8c7270cd8f8
David Fernandez <dfernandez@gitlab.com>
2020-08-26 11:10:12 +0200

    Update the spec

--- spec/workers/cleanup_container_repository_worker_spec.rb
+++ spec/workers/cleanup_container_repository_worker_spec.rb
@@ -12,20 +12,31 @@ RSpec.describe CleanupContainerRepositoryWorker, :clean_gitlab_redis_shared_stat
   let(:job_args) { [user_id, repository_id, params] }
 
   describe '#perform' do
+    let(:service) { instance_double(Projects::ContainerRepository::CleanupTagsService) }
+    let(:manifest) { { 'tags' => %w(A B C) } }
+
+    subject { described_class.new.perform(*job_args) }
+
     context 'bulk delete api' do
-      let(:params) { { key: 'value', 'container_expiration_policy' => false } }
+      let(:params) { { key: 'value', 'name_regex_delete' => '.*', 'container_expiration_policy' => false } }
 
-      include_examples 'an idempotent worker' do
-        it 'executes the destroy service' do
-          expect_next_instance_of(Projects::ContainerRepository::CleanupTagsService) do |service|
-            expect(service).to receive(:execute)
+      it 'executes the destroy service' do
+        expect(Projects::ContainerRepository::CleanupTagsService).to receive(:new)
+          .with(project, user, params.merge('container_expiration_policy' => false))
+          .and_return(service)
+
+        expect(service).to receive(:execute)
+
+        subject
+      end
+
+      it_behaves_like 'an idempotent worker' do
+        before do
+          allow_next_instance_of(ContainerRegistry::Client) do |client|
+            allow(client).to receive(:supports_tag_delete?).and_return(true)
+            allow(client).to receive(:repository_tags).and_return(manifest)
+            allow(client).to receive(:delete_if_exists).and_return(true)
           end
-
-          expect(Projects::ContainerRepository::CleanupTagsService).to receive(:new)
-            .with(project, user, params.merge('container_expiration_policy' => false))
-            .and_call_original
-
-          subject
         end
       end
 
@@ -43,85 +54,90 @@ RSpec.describe CleanupContainerRepositoryWorker, :clean_gitlab_redis_shared_stat
     end
 
     context 'container expiration policy' do
-      let(:params) { { key: 'value', 'container_expiration_policy' => true } }
-      let(:job_args) { [nil, repository_id, params] }
+      let(:params) { { key: 'value', 'name_regex_delete' => '.*', 'container_expiration_policy' => true } }
 
-      include_examples 'an idempotent worker' do
-        it 'executes the destroy service' do
-          expect_next_instance_of(Projects::ContainerRepository::CleanupTagsService) do |service|
-            expect(service).to receive(:execute)
+      it 'executes the destroy service' do
+        expect(Projects::ContainerRepository::CleanupTagsService).to receive(:new)
+          .and_return(service)
+
+        expect(service).to receive(:execute)
+
+        subject
+      end
+
+      it_behaves_like 'an idempotent worker' do
+        before do
+          allow_next_instance_of(ContainerRegistry::Client) do |client|
+            allow(client).to receive(:supports_tag_delete?).and_return(true)
+            allow(client).to receive(:repository_tags).and_return(manifest)
+            allow(client).to receive(:delete_if_exists).and_return(true)
           end
-
-          expect(Projects::ContainerRepository::CleanupTagsService).to receive(:new)
-            .with(project, nil, params.merge('container_expiration_policy' => true))
-            .and_call_original
-
-          subject
         end
       end
     end
 
     context 'with feature flag container_registry_expiration_policies_throttling enabled' do
       let(:params) { { 'container_expiration_policy' => true } }
-      let(:job_args) { [nil, repository_id, params] }
 
       before do
         stub_feature_flags(container_registry_expiration_policies_throttling: true)
-        allow_next_instance_of(Projects::ContainerRepository::CleanupTagsService) do |service|
-          allow(service).to receive(:execute)
+        allow(Projects::ContainerRepository::CleanupTagsService).to receive(:new).and_return(service)
+        allow(service).to receive(:execute)
+      end
+
+      context 'without a jids cache key' do
+        it 'do not access redis' do
+          Sidekiq.redis do |redis|
+            expect(redis).not_to receive(:lrem)
+          end
+
+          subject
         end
       end
 
-      include_examples 'an idempotent worker' do
-        context 'without a jids cache key' do
-          it 'do not access redis' do
-            Sidekiq.redis do |redis|
-              expect(redis).not_to receive(:lrem)
-            end
+      context 'with a jids cache key' do
+        let(:cache_key) { 'jids' }
+        let(:jid) { 1234567 }
+        let(:params) { { 'container_expiration_policy' => true, 'jids_cache_key' => cache_key } }
+        let(:expected_cache_size) { 0 }
 
-            subject
+        before do
+          Sidekiq.redis do |redis|
+            redis.rpush(cache_key, jid)
           end
         end
 
-        context 'with a jids cache key' do
-          let(:cache_key) { 'jids' }
-          let(:jid) { 1234567 }
-          let(:params) { { 'container_expiration_policy' => true, 'jids_cache_key' => cache_key } }
-          let(:expected_cache_size) { 0 }
+        after do
+          Sidekiq.redis do |redis|
+            expect(redis.llen(cache_key)).to eq(expected_cache_size)
+          end
+        end
 
-          before do
-            Sidekiq.redis do |redis|
-              redis.rpush(cache_key, jid)
-              expect(redis).to receive(:lrem)
-                .exactly(IdempotentWorkerHelper::WORKER_EXEC_TIMES).times
-                .and_call_original
-            end
+        it 'removes the jid from the cache' do
+          allow_next_instance_of(CleanupContainerRepositoryWorker) do |worker|
+            allow(worker).to receive(:jid).and_return(jid)
           end
 
-          after do
-            Sidekiq.redis do |redis|
-              expect(redis.llen(cache_key)).to eq(expected_cache_size)
-            end
+          Sidekiq.redis do |redis|
+            expect(redis).to receive(:lrem).and_call_original
           end
 
-          it 'removes the jid from the cache' do
+          subject
+        end
+
+        context 'not containing the jid' do
+          let(:expected_cache_size) { 1 }
+
+          it 'does not remove the other jids from the cache' do
             allow_next_instance_of(CleanupContainerRepositoryWorker) do |worker|
-              allow(worker).to receive(:jid).and_return(jid)
+              allow(worker).to receive(:jid).and_return(5555)
+            end
+
+            Sidekiq.redis do |redis|
+              expect(redis).to receive(:lrem).and_call_original
             end
 
             subject
-          end
-
-          context 'not containing the jid' do
-            let(:expected_cache_size) { 1 }
-
-            it 'does not remove the other jids from the cache' do
-              allow_next_instance_of(CleanupContainerRepositoryWorker) do |worker|
-                allow(worker).to receive(:jid).and_return(5555)
-              end
-
-              subject
-            end
           end
         end
       end
